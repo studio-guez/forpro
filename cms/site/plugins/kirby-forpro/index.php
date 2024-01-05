b7df58e6f60528bb432811c9254a91a3e5a0cbbb
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-05 22:58:15 +0100

    Add the capsule block in the plugin import

--- cms/site/plugins/kirby-forpro/index.php
+++ cms/site/plugins/kirby-forpro/index.php
@@ -7,5 +7,6 @@ Kirby::plugin('mediumsans/forpro', [
         'blocks/cta'            => __DIR__ . '/blueprints/blocks/cta.yml',
         'blocks/profiles'       => __DIR__ . '/blueprints/blocks/profiles.yml',
         'blocks/quote'          => __DIR__ . '/blueprints/blocks/quote.yml',
+        'blocks/capsule'        => __DIR__ . '/blueprints/blocks/capsule.yml',
     ]
 ]);
