03f4956af2eccd6636380ec4cf0ef24bd3f28a56
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-05 22:01:34 +0100

    Add cms app

---
+++ cms/site/plugins/kirby-forpro/index.php
@@ -0,0 +1,11 @@
+<?php
+
+Kirby::plugin('mediumsans/forpro', [
+    'blueprints' => [
+        'blocks/animated-list'  => __DIR__ . '/blueprints/blocks/animated-list.yml',
+        'blocks/cards'          => __DIR__ . '/blueprints/blocks/cards.yml',
+        'blocks/cta'            => __DIR__ . '/blueprints/blocks/cta.yml',
+        'blocks/profiles'       => __DIR__ . '/blueprints/blocks/profiles.yml',
+        'blocks/quote'          => __DIR__ . '/blueprints/blocks/quote.yml',
+    ]
+]);
