67e6e7a5a02b127366a95531a1810417f510c003
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-09 14:19:22 +0100

    feat: add forpro page, content tab and map block to the plugin linkin

--- cms/site/plugins/kirby-forpro/index.php
+++ cms/site/plugins/kirby-forpro/index.php
@@ -2,11 +2,14 @@
 
 Kirby::plugin('mediumsans/forpro', [
     'blueprints' => [
+        'pages/forpro'          => __DIR__ . '/blueprints/pages/forpro.yml',
+        'tabs/content'          => __DIR__ . '/blueprints/tabs/content.yml',
         'blocks/animated-list'  => __DIR__ . '/blueprints/blocks/animated-list.yml',
         'blocks/cards'          => __DIR__ . '/blueprints/blocks/cards.yml',
         'blocks/cta'            => __DIR__ . '/blueprints/blocks/cta.yml',
         'blocks/profiles'       => __DIR__ . '/blueprints/blocks/profiles.yml',
         'blocks/quote'          => __DIR__ . '/blueprints/blocks/quote.yml',
         'blocks/capsule'        => __DIR__ . '/blueprints/blocks/capsule.yml',
+        'blocks/map'            => __DIR__ . '/blueprints/blocks/map.yml',
     ]
 ]);
