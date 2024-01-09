ace82f553250668e1e38fd5c3d620bc60ef87df9
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-09 16:06:39 +0100

    feat: add appointment block

--- cms/site/plugins/kirby-forpro/index.php
+++ cms/site/plugins/kirby-forpro/index.php
@@ -2,8 +2,12 @@
 
 Kirby::plugin('mediumsans/forpro', [
     'blueprints' => [
+        'templates/forpo'       => __DIR__ . '/blueprints/templates/forpro.php',
+
         'pages/forpro'          => __DIR__ . '/blueprints/pages/forpro.yml',
+
         'tabs/content'          => __DIR__ . '/blueprints/tabs/content.yml',
+
         'blocks/animated-list'  => __DIR__ . '/blueprints/blocks/animated-list.yml',
         'blocks/cards'          => __DIR__ . '/blueprints/blocks/cards.yml',
         'blocks/cta'            => __DIR__ . '/blueprints/blocks/cta.yml',
