2ff12a28e78ed5fdc3034479ac5850cb447fd02b
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-11 18:50:56 +0100

    fix: remove template from the index

--- cms/site/plugins/kirby-forpro/index.php
+++ cms/site/plugins/kirby-forpro/index.php
@@ -2,12 +2,8 @@
 
 Kirby::plugin('mediumsans/forpro', [
     'blueprints' => [
-        'templates/forpo'       => __DIR__ . '/blueprints/templates/forpro.php',
-
         'pages/forpro'          => __DIR__ . '/blueprints/pages/forpro.yml',
-
         'tabs/content'          => __DIR__ . '/blueprints/tabs/content.yml',
-
         'blocks/cards'          => __DIR__ . '/blueprints/blocks/cards.yml',
         'blocks/cards-focus'    => __DIR__ . '/blueprints/blocks/cards-focus.yml',
         'blocks/cta'            => __DIR__ . '/blueprints/blocks/cta.yml',
@@ -19,5 +15,9 @@ Kirby::plugin('mediumsans/forpro', [
         'blocks/dropdown'       => __DIR__ . '/blueprints/blocks/dropdown.yml',
         'blocks/list'           => __DIR__ . '/blueprints/blocks/list.yml',
         'blocks/google-maps'    => __DIR__ . '/blueprints/blocks/google-maps.yml',
+    ],
+    'templates' => [
+        'forpro'                => __DIR__ . '/templates/forpro.php',
+        'forpro'                => __DIR__ . '/templates/forpro.json.php',
     ]
 ]);
