34f5ef37e16ed0a0f2f1455c65d2a951ab85a392
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-18 14:41:29 +0100

    fix : full refactor of the calendar app

--- booking/booking/urls.py
+++ booking/booking/urls.py
@@ -5,10 +5,7 @@ from django.urls import re_path, include, path
 
 from djradicale.views import WellKnownView
 
-from sharpness import views
-
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
     path('admin/', admin.site.urls),
     path('agenda/', include('agenda.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
@@ -17,16 +14,6 @@ urlpatterns = [
     re_path(r'^\.well-known/(?P<type>(caldav|carddav))$', WellKnownView.as_view(),
             name='djradicale_well-known'),
 ]
-
-# DJANGO DEBUG TOOLBAR
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
 
 # Branding Admin
 admin.site.site_title = "Calendrier"
