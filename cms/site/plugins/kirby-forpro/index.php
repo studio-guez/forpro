305b79c4734ae28bdbfb6d58d2a14c4bbff15fd3
azertypow <azertypow@gmail.com>
2024-01-18 10:51:13 +0100

    feat: move capsule to capsules Structure logic, change max length for title and description content

--- cms/site/plugins/kirby-forpro/index.php
+++ cms/site/plugins/kirby-forpro/index.php
@@ -4,12 +4,13 @@ Kirby::plugin('mediumsans/forpro', [
     'blueprints' => [
         'pages/forpro'          => __DIR__ . '/blueprints/pages/forpro.yml',
         'tabs/content'          => __DIR__ . '/blueprints/tabs/content.yml',
+
         'blocks/cards'          => __DIR__ . '/blueprints/blocks/cards.yml',
         'blocks/cards-focus'    => __DIR__ . '/blueprints/blocks/cards-focus.yml',
         'blocks/cta'            => __DIR__ . '/blueprints/blocks/cta.yml',
         'blocks/profiles'       => __DIR__ . '/blueprints/blocks/profiles.yml',
         'blocks/quote'          => __DIR__ . '/blueprints/blocks/quote.yml',
-        'blocks/capsule'        => __DIR__ . '/blueprints/blocks/capsule.yml',
+        'blocks/capsules'        => __DIR__ . '/blueprints/blocks/capsules.yml',
         'blocks/map'            => __DIR__ . '/blueprints/blocks/map.yml',
         'blocks/body'           => __DIR__ . '/blueprints/blocks/body.yml',
         'blocks/dropdown'       => __DIR__ . '/blueprints/blocks/dropdown.yml',
