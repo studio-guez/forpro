e88d8327116da2aeaf4556b1889639c2da157c78
azertypow <azertypow@gmail.com>
2024-01-23 15:50:55 +0100

    feat: init app AppNav component

---
+++ website/src/lib/components/AppNav.svelte
@@ -0,0 +1,29 @@
+<nav class="s-app-nav app-flex app-flex--justify_space-between app-flex--align_center">
+    <div class="app-flex__basis-auto"
+    >
+        <div>FORPRO</div>
+    </div>
+
+    <div class="app-flex__basis-auto"
+    >
+        <div class="app-flex app-flex--gap_regular app-flex--align_center"
+        >
+            <div>Prendre RDV</div>
+            <div>social</div>
+            <div>navIcon</div>
+        </div>
+    </div>
+</nav>
+
+<style lang="scss" >
+    .s-app-nav {
+      height: var(--app-nav_height);
+      padding: 1rem;
+      box-sizing: border-box;
+      border-radius: var(--app-nav_height);
+      flex-direction: row;
+      justify-content: space-between;
+      border: solid 3px black;
+      background: white;
+    }
+</style>
