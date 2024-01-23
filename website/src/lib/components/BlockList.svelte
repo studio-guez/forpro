5ccff0f6d5df1b91c1e32bb6d2b80648d92d3de2
azertypow <azertypow@gmail.com>
2024-01-23 22:58:13 +0100

    feat: all blocks components types

---
+++ website/src/lib/components/BlockList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type {IList} from "$lib/interfaces/cmsApiResponse";
+
+    export let content: IList;
+</script>
+
+<div>
+    <h2>list</h2>
+    {content.type}
+</div>
