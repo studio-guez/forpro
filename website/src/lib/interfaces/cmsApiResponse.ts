c8c6b94d9b1577a2081f283a5472ef5e1339b221
azertypow <azertypow@gmail.com>
2024-01-23 22:28:39 +0100

    feat: clean CMS <-> WebSite api interface

--- website/src/lib/interfaces/cmsApiResponse.ts
+++ website/src/lib/interfaces/cmsApiResponse.ts
@@ -1,3 +1,12 @@
+/**
+ * PAGE
+ */
+export interface IPage {
+    options: IOptions
+    body: IBody
+    seo: ISeo
+}
+
 export interface IOptions {
     showMenu: boolean
     showNewsletter: boolean
@@ -8,6 +17,44 @@ export interface IOptions {
     }
 }
 
+export interface IBody {
+    [key: string]: {
+        image: IImage[]
+        content: ICta | IQuote | ICapsules | ICards | IProfiles | IList | IDropdown | ICardsFocus | IHtmlContent
+    }
+}
+
+
+export interface IBlock {
+    id: string
+    isHidden: boolean
+    type: 'cta' | 'quote' | 'capsules' | 'cards' | 'profiles' | 'list' | 'dropdown' | 'cards-focus' | 'body'
+}
+
+export interface ISeo {
+    metaTemplate: string
+    metaDescription: string
+    metaAuthor: string
+    metaImage: string
+    metaPhoneNumber: string
+    ogTemplate: string
+    ogDescription: string
+    ogImage: string
+    ogSiteName: string
+    twitterTemplate: string
+    twitterDescription: string
+    twitterImage: string
+    twitterCardType: string
+    twitterSite: string
+    twitterCreator: string
+}
+
+
+
+
+/**
+ * BLOCK IMAGE
+ */
 export interface IImage {
     caption: string | null
     alt: string | null
@@ -26,29 +73,51 @@ export interface IImage {
     }
 }
 
-export interface IContent {
-    image: string[]
-    text: string
-    link: string
-    backgroundcolor: string
-    textcolor: string
-    styles: string
+
+
+
+
+/**
+ * CTA
+ */
+export interface ICta extends IBlock {
+    content: {
+        image: string[]
+        text: string
+        link: string
+        backgroundcolor: string
+        textcolor: string
+        styles: 'style1' | 'style2'
+    }
 }
 
-export interface ICta {
-    content: IContent
-    id: string
-    isHidden: boolean
-    type: 'cta'
-}
 
-export interface IQuote {
+
+
+
+/**
+ * QUOTE
+ */
+export interface IQuote extends IBlock {
     content: {
         text: string
     }
-    id: string
-    isHidden: boolean
     type: 'quote'
+}
+
+
+
+
+
+/**
+ * CAPSULE
+ */
+export interface ICapsules extends IBlock {
+    content: {
+        style: 'style1' | 'style2'
+        capsules: ICapsule[]
+    }
+    type: 'capsules'
 }
 
 export interface ICapsule {
@@ -57,24 +126,118 @@ export interface ICapsule {
     image: string[]
 }
 
-export interface ICapsules {
+
+
+
+
+/**
+ * CARD
+ */
+export interface ICards extends IBlock {
     content: {
-        style: string
-        capsules: ICapsule[]
-    }
-    id: string
-    isHidden: boolean
-    type: 'capsules'
+        style: 'style1' | 'style2',
+        cards: ICard[]
+    },
+    type: 'cards'
 }
 
-export interface IBody {
-    [key: string]: {
-        image: IImage[]
-        content: ICta | IQuote | ICapsules
-    }
+export interface ICard {
+    title: "",
+    image: IImage[],
+    link: "",
+    text: ""
 }
 
-export interface IPage {
-    options: IOptions
-    body: IBody
+
+
+
+
+/**
+ * PROFILE
+ */
+interface IProfiles extends IBlock {
+    content: {
+        style: 'style1' | 'style2';
+        profiles: IProfile[];
+    },
+    type: 'profiles'
+}
+
+
+interface IProfile {
+    title: string;
+    subtitle: string;
+    description: string;
+    images: string[];
+    lien: string;
+    mailto: string;
+}
+
+
+/**
+ * BLOCK LIST
+ */
+
+interface IList extends IBlock {
+    content: {
+        list: ListItem[];
+    }
+    type: 'list'
+}
+
+interface ListItem {
+    key: string;
+    value: string;
+}
+
+
+
+
+
+/**
+ * BLOCK DROPDOWN
+ */
+interface IDropdown extends IBlock {
+    content: {
+        dropdown: IDropdownItem[];
+    }
+    type: 'dropdown'
+}
+
+interface IDropdownItem {
+    title: string;
+    content: string;
+    link: string;
+}
+
+
+
+
+
+
+/**
+ * BLOCK CARDSFOCUS
+ * */
+interface ICardsFocus extends IBlock {
+    "content": {
+        "style": "style1",
+        "cards": ICardFocusItem[]
+    },
+    type: 'cards-focus'
+}
+
+interface ICardFocusItem {
+    "title": string,
+    "subtitle": string,
+    "link": string
+}
+
+/**
+ * BODY
+ */
+interface IHtmlContent extends IBlock {
+    "content": {
+        "text": "<p>bonjour le text</p><p>bonjour me monde</p>"
+    }
+    type: 'body'
 }
