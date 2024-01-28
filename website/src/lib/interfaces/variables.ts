8cfb411b6a143df32cb7baf7597c43d6141e42a4
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-28 12:48:47 +0100

    chore: add types and refactor

--- website/src/lib/interfaces/variables.ts
+++ website/src/lib/interfaces/variables.ts
@@ -1,4 +1,19 @@
 export interface Variables {
     readonly BASE_CMS_URL: string;
-    readonly BASE_CALENDAR_URL: string;
+    readonly BASE_EASYAPPOINTMENTS_URL: string;
+}
+
+export interface Slot {
+    readonly date: string;
+    readonly time: string;
+}
+
+export interface AppointmentDetails {
+    readonly date: string;
+    readonly time: string;
+}
+
+export interface BookingCMSResponse {
+    readonly headline: string;
+    readonly description: string;
 }
