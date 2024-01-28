b2e89357de1498f69ae4ac1722c3d46e238f1ad9
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-28 12:52:48 +0100

    feat: add booking tab on the cms to enable customer to manage the content of it

--- cms/site/plugins/kirby-forpro/index.php
+++ cms/site/plugins/kirby-forpro/index.php
@@ -3,7 +3,9 @@
 Kirby::plugin('mediumsans/forpro', [
     'blueprints' => [
         'pages/forpro'          => __DIR__ . '/blueprints/pages/forpro.yml',
+
         'tabs/content'          => __DIR__ . '/blueprints/tabs/content.yml',
+        'tabs/booking'          => __DIR__ . '/blueprints/tabs/booking.yml',
 
         'blocks/cards'          => __DIR__ . '/blueprints/blocks/cards.yml',
         'blocks/cards-focus'    => __DIR__ . '/blueprints/blocks/cards-focus.yml',
@@ -20,5 +22,19 @@ Kirby::plugin('mediumsans/forpro', [
     'templates' => [
         'forpro'                => __DIR__ . '/templates/forpro.php',
         'forpro'                => __DIR__ . '/templates/forpro.json.php',
-    ]
+    ],
+      'routes' => function ($kirby) {
+        return [
+            [
+              'pattern' => 'booking',
+              'method'  => 'GET',
+              'action'  => function () use ($kirby) {
+                  return [
+                    'headline'    => $kirby->site()->bookingHeadline()->value(),
+                    'description' => $kirby->site()->bookingDescription()->value(),
+                  ];
+              }
+            ],
+        ];
+    },
 ]);
