90a45ea13a60c38fa26a0e62805258e769255d1c
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-28 12:48:47 +0100

    chore: add types and refactor

--- website/src/lib/utils/constants.ts
+++ website/src/lib/utils/constants.ts
@@ -4,8 +4,11 @@ export const BASE_CMS_URL: string = import.meta.env.DEV
     ? import.meta.env.VITE_BASE_CMS_URL_DEV
     : import.meta.env.VITE_BASE_CMS_URL_PROD;
 
-export const BASE_CALENDAR_URL: string = import.meta.env.DEV
-    ? import.meta.env.VITE_BASE_CALENDAR_URL_DEV
-    : import.meta.env.VITE_BASE_CALENDAR_URL_PROD;
+export const BASE_EASYAPPOINTMENTS_URL: string = import.meta.env.DEV
+    ? import.meta.env.VITE_BASE_EASYAPPOINTMENTS_URL_DEV
+    : import.meta.env.VITE_BASE_EASYAPPOINTMENTS_URL_PROD;
 
-export const variables: Variables = { BASE_CMS_URL: BASE_CMS_URL, BASE_CALENDAR_URL: BASE_CALENDAR_URL };
+export const variables: Variables = {
+    BASE_CMS_URL: BASE_CMS_URL,
+    BASE_EASYAPPOINTMENTS_URL: BASE_EASYAPPOINTMENTS_URL
+};
