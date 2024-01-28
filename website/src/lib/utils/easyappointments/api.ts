ca765a007ef0bd53ee2049f9f5776e693362a44f
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-28 20:38:40 +0100

    chore: refactor

--- website/src/lib/utils/easyappointments/api.ts
+++ website/src/lib/utils/easyappointments/api.ts
@@ -1,21 +1,78 @@
-import type {Slot, Service, Provider} from '$lib/interfaces/variables';
-import {getAppointmentsUrl, getProvidersUrl, getServicesFromProviderUrl} from "$lib/utils/easyappointments/urls";
+import type {Slot, Service, Provider, Customer, Appointment} from '$lib/interfaces/variables';
+import {
+    getAppointmentsUrl,
+    getAvailabilitiesUrl, getCustomersFromEmailUrl,
+    getCustomersUrl,
+    getServicesFromProviderUrl
+} from "$lib/utils/easyappointments/urls";
 import {fetchFromAPI, getHeaders} from "$lib/utils/shared";
 
-export const fetchAvailableSlots = async (date: string, serviceId: number, providerId: number): Promise<Slot[]> => {
+export const getAvailableSlots = async (date: string, serviceId: number, providerId: number): Promise<Slot[]> => {
     const body = {date: date, providerId: providerId, serviceId: serviceId};
 
-    const request = new Request(getAppointmentsUrl(serviceId, date), {
+    const request = new Request(getAvailabilitiesUrl(providerId, serviceId, date), {
         headers: getHeaders(),
     });
 
     return fetchFromAPI<Slot[]>(request, 'Failed to fetch available slots');
 }
 
-export const fetchServicesFromProvider = async (providerId: string): Promise<Service[]> => {
+export const getServicesFromProvider = async (providerId: string): Promise<Service[]> => {
     const request = new Request(getServicesFromProviderUrl(providerId), {
         headers: getHeaders(),
     });
 
     return fetchFromAPI<Service[]>(request, 'Failed to fetch services');
+}
+
+export const getCustomer = async (email: string): Promise<Customer> => {
+    const request = new Request(`${getCustomersFromEmailUrl(email)}`, {
+        headers: getHeaders(),
+    });
+
+    return fetchFromAPI<Customer>(request, 'Failed to fetch customer');
+}
+
+export const createCustomer = async (email: string, firstname: string, lastname: string, phone: string): Promise<Customer> => {
+    const body = {
+        email: email,
+        firstName: firstname,
+        lastName: lastname,
+        phone: phone,
+        city: "",
+        zip: "",
+        timezone: "",
+        language: "",
+        notes: ""
+    };
+
+    const request = new Request(getCustomersUrl(), {
+        method: 'POST',
+        headers: getHeaders(),
+        body: JSON.stringify(body)
+    });
+
+    return fetchFromAPI<Customer>(request, 'Failed to create customer');
+}
+
+export const createAppointment = async (start: string, end: string, serviceId: string, providerId: string, customerId: string, notes: string): Promise<Appointment> => {
+    const body = {
+        start: start,
+        end: end,
+        location: '',
+        notes: notes,
+        serviceId: serviceId,
+        providerId: providerId,
+        customerId: customerId,
+    };
+
+    console.log(body);
+
+    const request = new Request(getAppointmentsUrl(), {
+        method: 'POST',
+        headers: getHeaders(),
+        body: JSON.stringify(body)
+    });
+
+    return fetchFromAPI<Appointment>(request, 'Failed to create appointment');
 }
--- website/src/lib/utils/easyappointments/urls.ts
+++ website/src/lib/utils/easyappointments/urls.ts
@@ -2,14 +2,22 @@ import {EASYAPPOINTMENTS_BASE_URL} from "$lib/utils/constants";
 
 const api_v1 = "index.php/api/v1";
 
-export const getAppointmentsUrl = (providerId: number, servicedId: number, date: string): string => {
+export const getAvailabilitiesUrl = (providerId: string, servicedId: string, date: string): string => {
     return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/availabilities?providerId=${providerId}&serviceId=${servicedId}&date=${date}`;
-}
-
-export const getProvidersUrl = (providerId: string): string => {
-    return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/providers?providerId=${providerId}`;
 }
 
 export const getServicesFromProviderUrl = (providerId: string): string => {
     return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/services?providerId=${providerId}`;
+}
+
+export const getAppointmentsUrl = (): string => {
+    return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/appointments`;
+}
+
+export const getCustomersFromEmailUrl = (email: string): string => {
+    return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/customers?email=${email}`;
+}
+
+export const getCustomersUrl = (): string => {
+    return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/customers`;
 }
