3a2a7662a045faf864153d4b5ac17630b024584e
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-28 17:24:15 +0100

    chore: inline returns

--- website/src/lib/utils/easyappointments/urls.ts
+++ website/src/lib/utils/easyappointments/urls.ts
@@ -1,21 +1,15 @@
-import {
-    EASYAPPOINTMENTS_BASE_URL,
-    EASYAPPOINTMENTS_PROVIDER_ID
-} from "$lib/utils/constants";
+import {EASYAPPOINTMENTS_BASE_URL} from "$lib/utils/constants";
 
 const api_v1 = "index.php/api/v1";
 
-export const getAppointmentsUrl = (servicedId: number, date: string): string => {
-    const url = `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/availabilities?providerId=${EASYAPPOINTMENTS_PROVIDER_ID}&serviceId=${servicedId}&date=${date}`;
-    return url;
+export const getAppointmentsUrl = (providerId: number, servicedId: number, date: string): string => {
+    return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/availabilities?providerId=${providerId}&serviceId=${servicedId}&date=${date}`;
 }
 
 export const getProvidersUrl = (providerId: string): string => {
-    const url = `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/providers?providerId=${providerId}`;
-    return url;
+    return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/providers?providerId=${providerId}`;
 }
 
 export const getServicesFromProviderUrl = (providerId: string): string => {
-    const url = `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/services?providerId=${providerId}`;
-    return url;
+    return `${EASYAPPOINTMENTS_BASE_URL}/${api_v1}/services?providerId=${providerId}`;
 }
