c64db4410f7aa8028201670035ef349f06a9cd65
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-28 17:24:47 +0100

    fix: redirect to the right handle if we get to the root of the booking app

---
+++ website/src/routes/(booking)/rendez-vous/+page.server.ts
@@ -0,0 +1,6 @@
+import type {PageServerLoad} from './$types';
+import {redirect} from "@sveltejs/kit";
+
+export const load: PageServerLoad = async ({ params, fetch }) => {
+    redirect(302, '/rendez-vous/2');
+};
