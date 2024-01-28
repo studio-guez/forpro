8b8973657b75cb5233a2cf8e59a21e4ce132f2f1
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-28 17:23:58 +0100

    chore: refactor and add env variables to manage

--- website/src/lib/utils/constants.ts
+++ website/src/lib/utils/constants.ts
@@ -1,14 +1,17 @@
 import type { Variables } from '$lib/interfaces/variables';
 
-export const BASE_CMS_URL: string = import.meta.env.DEV
-    ? import.meta.env.VITE_BASE_CMS_URL_DEV
-    : import.meta.env.VITE_BASE_CMS_URL_PROD;
+export const CMS_BASE_URL: string = import.meta.env.DEV
+    ? import.meta.env.VITE_CMS_BASE_URL_DEV
+    : import.meta.env.VITE_CMS_BASE_URL_PROD;
 
-export const BASE_EASYAPPOINTMENTS_URL: string = import.meta.env.DEV
-    ? import.meta.env.VITE_BASE_EASYAPPOINTMENTS_URL_DEV
-    : import.meta.env.VITE_BASE_EASYAPPOINTMENTS_URL_PROD;
+export const EASYAPPOINTMENTS_BASE_URL: string = import.meta.env.DEV
+    ? import.meta.env.VITE_EASYAPPOINTMENTS_BASE_URL_DEV
+    : import.meta.env.VITE_EASYAPPOINTMENTS_BASE_URL_PROD;
+
+export const EASYAPPOINTMENTS_API_TOKEN: string = import.meta.env.VITE_EASYAPPOINTMENTS_API_TOKEN;
 
 export const variables: Variables = {
-    BASE_CMS_URL: BASE_CMS_URL,
-    BASE_EASYAPPOINTMENTS_URL: BASE_EASYAPPOINTMENTS_URL
+    CMS_BASE_URL: CMS_BASE_URL,
+    EASYAPPOINTMENTS_BASE_URL: EASYAPPOINTMENTS_BASE_URL,
+    EASYAPPOINTMENTS_API_TOKEN: EASYAPPOINTMENTS_API_TOKEN,
 };
