6f325ee605999e795b055f3e963ba51620330bca
azertypow <azertypow@gmail.com>
2024-01-29 16:33:41 +0100

    feat: use dynamique page move page in [slug] directory

--- website/src/routes/[slug]/+page.ts
+++ website/src/routes/[slug]/+page.ts
@@ -1,11 +1,11 @@
 import {fetchFromAPI} from "$lib/utils/shared";
 import {type IPage} from "$lib/interfaces/cmsApiResponse";
 import {CMS_BASE_URL} from "$lib/utils/constants";
-import type {PageLoad} from "../../.svelte-kit/types/src/routes/[slug]/$types";
+import type {PageLoad} from "../../../.svelte-kit/types/src/routes/[slug]/$types";
 
 export const load: PageLoad = async ({params}) => {
 
-    const request = new Request(`${CMS_BASE_URL}page-exemple.json`, {
+    const request = new Request(`${CMS_BASE_URL}${params.slug}.json`, {
         method: 'GET',
     })
 
