c00da94ae80ce2e72c286f724432c8940abceb61
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-30 13:21:50 +0100

    feat: integrates the managing of 404 errors

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -11,7 +11,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
 	},
 };
 
