a527a39d2f4fb477dda9b6d297f1a2877d39bacd
azertypow <azertypow@gmail.com>
2024-01-30 10:56:21 +0100

    feat: page layout

---
+++ website/src/routes/+page.ts
@@ -0,0 +1,13 @@
+import {fetchFromAPI} from "$lib/utils/shared";
+import {type IPage} from "$lib/interfaces/cmsApiResponse";
+import {CMS_BASE_URL} from "$lib/utils/constants";
+import type {PageLoad} from "../../.svelte-kit/types/src/routes/[slug]/$types";
+
+export const load: PageLoad = async ({params}) => {
+
+    const request = new Request(`${CMS_BASE_URL}home.json`, {
+        method: 'GET',
+    })
+
+    return await fetchFromAPI<IPage>(request, 'Failed to fetch page data')
+}
