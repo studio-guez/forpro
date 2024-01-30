4a7f272a72239a4ba9942cea5a32348f0e631e9f
azertypow <azertypow@gmail.com>
2024-01-30 11:06:50 +0100

    feat: CTA Block

--- website/src/lib/components/BlockCta.svelte
+++ website/src/lib/components/BlockCta.svelte
@@ -1,10 +1,55 @@
 <script lang="ts">
-    import type {ICta} from "$lib/interfaces/cmsApiResponse";
+    import type {ICta, IImage} from "$lib/interfaces/cmsApiResponse";
 
     export let content: ICta;
+    export let image: IImage[];
+
 </script>
 
-<div>
-    <h2>CTA</h2>
-    {content.type}
-</div>
+<a class="s-block-cta"
+   class:has-icon={image.length > 0}
+   href="{content.content.link}"
+   style="
+        --s-cat-background-color: {content.content.backgroundcolor};
+        --s-cat-color: {content.content.textcolor};
+    "
+>
+    {#if (image.length > 0)}
+        <img class="s-block-cta__icon"
+             src={image[0].url}
+             alt="icon illustratif pour le bouton"/>
+    {/if}
+    <div>
+        {content.content.text}
+    </div>
+</a>
+
+
+<style lang="scss">
+    .s-block-cta {
+      background: var(--s-cat-background-color);
+      color: var(--s-cat-color);
+      padding: max(2rem, 4vw);
+      align-items: center;
+      box-sizing: border-box;
+      border-radius: max(1rem, 2vw);
+      text-decoration: none;
+      text-align: center;
+      display: block;
+      line-height: 1em;
+      font-size: max(4rem, 6vw);
+
+      &.has-icon {
+        background: transparent;
+        line-height: 1em;
+        font-size: 2rem;
+      }
+    }
+
+    .s-block-cta__icon {
+      display: block;
+      height: 8rem;
+      margin: auto;
+    }
+
+</style>
