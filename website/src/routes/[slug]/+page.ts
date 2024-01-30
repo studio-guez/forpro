d0e9e0b840e086d6297c0337d076726046b2a9c4
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-30 20:45:28 +0100

    chore: migrate everything to the node-adapter

--- website/src/routes/[slug]/+page.ts
+++ website/src/routes/[slug]/+page.ts
@@ -3,6 +3,8 @@ import {type IPage} from "$lib/interfaces/cmsApiResponse";
 import {CMS_BASE_URL} from "$lib/utils/constants";
 import type {PageLoad} from "../../../.svelte-kit/types/src/routes/[slug]/$types";
 
+export const prerender = false;
+
 export const load: PageLoad = async ({params}) => {
 
     const request = new Request(`${CMS_BASE_URL}${params.slug}.json`, {
