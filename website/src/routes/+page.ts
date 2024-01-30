9293ea036f6b47b107d132d2a26a871e925b8fbe
Samuel Cardoso <samuel@mediumsans.studio>
2024-01-30 13:25:08 +0100

    chore: remove breaking build code

--- website/src/routes/+page.ts
+++ website/src/routes/+page.ts
@@ -1,6 +1,1 @@
-import {fetchAPIPageContent} from "$lib/utils/shared";
-import {type IPage} from "$lib/interfaces/cmsApiResponse";
-
-export async function  load(): Promise<IPage> {
-    return await fetchAPIPageContent('page-exemple') satisfies IPage
-}
+// To overwrite was causing build errors
