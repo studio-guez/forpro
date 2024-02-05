bda046e73f49981b4ba8283f35313681db1ecdc0
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-05 10:13:48 +0100

    fix: kirby cms config for pro

--- cms/site/config/config.php
+++ cms/site/config/config.php
@@ -5,6 +5,7 @@ header('Access-Control-Allow-Origin: *');
 return [
     'debug' => true,
     'url_frontend' => 'https://forpro-website.sdrvl.ch',
+    'url' => 'https://forpro-website.sdrvl.ch',
     'routes' => [
         [
             'pattern' => '/',
