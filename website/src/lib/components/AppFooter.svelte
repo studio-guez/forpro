27c46a267ac5e23c3b0e4c7ffedbc319fc317053
azertypow <azertypow@gmail.com>
2024-02-05 13:13:43 +0100

    feat: link footer to CMS api data

--- website/src/lib/components/AppFooter.svelte
+++ website/src/lib/components/AppFooter.svelte
@@ -1,20 +1,13 @@
-<footer class="s-footer app-flex app-width-gutter"
->
-    <div class="app-flex__basis-1-2 app-width-gutter">
-        <h5>Fondation ForPro</h5>
-        <p>Rte de la Galaise
-            <br>23 1228Plan-les-Ouates
-            <br><a href="tel:+41763584223">+41 76 358 42 23</a>
-        </p>
-        <p>
-            <a href="mailto:info@for-pro.ch">info@for-pro.ch</a>
-        </p>
+<script lang="ts">
+import {siteInfo} from "../../store";
+</script>
 
-        <p>
-            LOREM! Vous avez une question ou un commentaire? Vous souhaitez participer à l’aventure en pro- posant une idée? Écrivez-nous pour un premier échange. Si vous souhaitez simplement être in- formé·e de l’avancement du projet, vous pouvez vous inscrire à notre newsletter en cliquant ici.
-        </p>
+<footer class="s-footer app-grid--column-2 app-width-gutter app-grid--gap_regular"
+>
+    <div class="app-typo_text-content">
+        {@html $siteInfo.footer}
     </div>
-    <div class="app-flex__basis-1-2 app-width-gutter">
+    <div>
         <p>
             right content
         </p>
