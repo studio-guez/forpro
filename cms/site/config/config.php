29101c7307109105be1a230389199b30ac705876
azertypow <azertypow@gmail.com>
2024-02-05 07:55:34 +0100

    feat: cms

--- cms/site/config/config.php
+++ cms/site/config/config.php
@@ -5,4 +5,12 @@ header('Access-Control-Allow-Origin: *');
 return [
     'debug' => true,
     'url_frontend' => 'https://forpro-website.sdrvl.ch',
+    'routes' => [
+        [
+            'pattern' => '/',
+            'action' => function() {
+                go('/panel');
+            }
+        ]
+    ]
 ];
