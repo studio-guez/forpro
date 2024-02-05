727b9eca60606108258b93b4dd5e9dc097a644c7
azertypow <azertypow@gmail.com>
2024-02-05 13:15:07 +0100

    feat: animated block

---
+++ website/src/lib/components/BlockAnimatedList.svelte
@@ -0,0 +1,20 @@
+<script>
+import {LottiePlayer} from "@lottiefiles/svelte-lottie-player";
+</script>
+
+<div class="s-animated-list">
+    <LottiePlayer
+            src="/lottie/desktop-model-jeune_lottie.json"
+            autoplay="{true}"
+    />
+</div>
+
+<style lang="scss">
+    .s-animated-list {
+
+        :global(.lottie-player svg) {
+          height: 80vh !important;
+        }
+    }
+
+</style>
