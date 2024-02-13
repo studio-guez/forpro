7c857e34617483f277f8e0b8e6057da688c71e52
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 00:16:00 +0100

    feat: add necessary service dialogs

---
+++ cms/site/plugins/kirby-calendars/dialogs/service/create.php
@@ -0,0 +1,22 @@
+<?php
+
+use MediumSans\KirbyCalendars\Service;
+
+return [
+    'pattern' => 'service/(:any)/create',
+    'load'    => function () {
+        return [
+            'component' => 'k-form-dialog',
+            'props'     => [
+                'fields'        => require __DIR__ . '/fields.php',
+                'submitButton'  => t('create'),
+                'size'          => 'large',
+            ],
+        ];
+    },
+    'submit' => function ($calendarId) {
+        $input = get();
+        $input['calendar_id'] = $calendarId;
+        return Service::create($input);
+    }
+];
