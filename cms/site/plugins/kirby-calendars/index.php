bbcdb91e1f18732c3871da522cca45d67f89842b
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 00:14:46 +0100

    feat: add calendars plugin essentials

---
+++ cms/site/plugins/kirby-calendars/index.php
@@ -0,0 +1,53 @@
+<?php
+
+@include_once __DIR__ . '/vendor/autoload.php';
+
+load([
+    'MediumSans\KirbyCalendars\Calendar' => __DIR__ . '/classes/Calendar.php',
+    'MediumSans\KirbyCalendars\Schedule' => __DIR__ . '/classes/Schedule.php',
+    'MediumSans\KirbyCalendars\Service' => __DIR__ . '/classes/Service.php',
+    'MediumSans\KirbyCalendars\Database' => __DIR__ . '/classes/Database.php',
+]);
+
+Kirby::plugin('medium-sans/kirby-calendars', [
+    'options' => [
+        'google_calendar_api_key' => 'AIzaSyBspHKYr_3MOpeGa7oGuUU15IeaCi0uetU',
+    ],
+    'areas' => [
+        'calendars' => function () {
+            return [
+                'label' => 'Calendriers',
+                'icon' => 'calendar',
+                'menu' => true,
+                'link' => 'kirby-calendars/calendars',
+                'view' => 'k-calendars-view',
+                'views' => [
+                    require __DIR__ . '/views/calendars.php',
+                    require __DIR__ . '/views/services.php',
+                    require __DIR__ . '/views/schedules.php',
+                ],
+                'dialogs' => [
+                    // CALENDAR
+                    require __DIR__ . '/dialogs/calendar/fields.php',
+                    require __DIR__ . '/dialogs/calendar/create.php',
+                    require __DIR__ . '/dialogs/calendar/delete.php',
+                    require __DIR__ . '/dialogs/calendar/edit.php',
+                    // SERVICE
+                    require __DIR__ . '/dialogs/service/fields.php',
+                    require __DIR__ . '/dialogs/service/create.php',
+                    require __DIR__ . '/dialogs/service/delete.php',
+                    require __DIR__ . '/dialogs/service/edit.php',
+                    // SCHEDULE
+                    require __DIR__ . '/dialogs/schedule/fields.php',
+                    require __DIR__ . '/dialogs/schedule/create.php',
+                    require __DIR__ . '/dialogs/schedule/delete.php',
+                    require __DIR__ . '/dialogs/schedule/edit.php',
+                ],
+                'dropdowns' => [
+                    require __DIR__ . '/dropdowns/calendar.php'
+                ],
+            ];
+        },
+    ],
+    'api' => require __DIR__ . '/routes/index.php',
+]);
