4c3894482aacaef0d9e532a7fb273ae6d3a6768b
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 00:15:32 +0100

    feat: add necessary calendar dialogs

---
+++ cms/site/plugins/kirby-calendars/dialogs/calendar/fields.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'name' => [
+        'label' => 'Nom',
+        'type' => 'text'
+    ],
+    'description' => [
+        'label' => 'Description',
+        'type' => 'textarea',
+        'buttons' => false
+    ],
+    'location' => [
+        'label' => 'Localisation',
+        'type' => 'text'
+    ],
+];
