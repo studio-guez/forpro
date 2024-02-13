ac8ffdf7e55d3fa6148633bba9b2e956dddf8375
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-13 20:45:04 +0100

    chore: remove booking app 🕊️

--- booking/django_csv_exports/__init__.py
+++
@@ -1,3 +0,0 @@
-"<Include a description of your project>"
-
-__version__ = '1.0.5'
--- booking/static/js/description_animate.js
+++
@@ -1,9 +0,0 @@
-$('.carrousel-row').slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    infinite: true,
-    speed: 250,
-    fade: true,
-    cssEase: 'linear'
-  });
