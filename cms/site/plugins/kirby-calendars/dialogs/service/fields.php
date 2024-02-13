80a396e5002a10db4be06ebcccb4d988bf1d9132
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 00:16:00 +0100

    feat: add necessary service dialogs

---
+++ cms/site/plugins/kirby-calendars/dialogs/service/fields.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'name' => [
+        'label' => 'Nom',
+        'type' => 'text'
+    ],
+    'description' => [
+        'label' => 'Description',
+        'type' => 'textarea',
+        'buttons' => false
+    ],
+    'duration' => [
+        'label' => 'Durée',
+        'type' => 'time',
+    ],
+];
