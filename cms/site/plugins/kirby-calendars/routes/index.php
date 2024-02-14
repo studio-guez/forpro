1b6bb783b6c5ec41ccf9c756a7d36b4c768922c8
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 11:04:09 +0100

    feat: add get-slots and add-slot route

--- cms/site/plugins/kirby-calendars/routes/index.php
+++ cms/site/plugins/kirby-calendars/routes/index.php
@@ -1,30 +1,49 @@
 <?php
 
+use Kirby\Data\Json;
+use Kirby\Http\Response;
 use MediumSans\KirbyCalendars\Calendar;
 
 return [
-    'routes' => function () {
-        return [
-            [
-                'pattern' => 'kirby-calendars/calendar/(:any)/update',
-                'method' => 'GET',
-                'action' => function (string $id) {
-                    return Calendar::update($id, get());
-                }
-            ],
-            [
-                'pattern' => 'kirby-calendars/get-slots/',
-                'method' => 'POST',
-                'action' => function () {
-                    $data = get();
+    [
+        'pattern' => 'kirby-calendars/calendar/(:any)/update',
+        'method' => 'GET',
+        'action' => function (string $id) {
+            return Calendar::update($id, get());
+        }
+    ],
+    [
+        'pattern' => 'kirby-calendars/get-slots/(:any)/(:any)/(:any)',
+        'method' => 'GET',
+        'action' => function ($calendarId, $serviceId, $date) {
+            return response::json(
+                Json::encode(
+                    Calendar::getFreeSlots($calendarId, $serviceId, $date)
+                )
+            );
+        }
+    ],
+    [
+        'pattern' => 'kirby-calendars/add-slot',
+        'method' => 'POST',
+        'action' => function () {
+            $data = get();
 
-                    $calendarId = $data['calendarId'];
-                    $serviceId = $data['serviceId'];
-                    $date = $data['date'];
+            $calendarId = $data['calendarId'];
+            $serviceId = $data['serviceId'];
+            $date = $data['date'];
+            $infos = $data['infos'];
 
-                    return Calendar::getFreeSlots($calendarId, $serviceId, $date);
-                }
-            ],
-        ];
-    }
+            return response::json(
+                Json::encode(
+                    Calendar::addEvent(
+                        $calendarId,
+                        $serviceId,
+                        $date,
+                        $infos
+                    )
+                )
+            );
+        }
+    ],
 ];
