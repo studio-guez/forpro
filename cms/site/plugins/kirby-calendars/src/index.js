655f05b8f2ad563f14b0bbccf2815fe9b2ed3f1c
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 11:04:46 +0100

    feat: map EventsView to the plugin

--- cms/site/plugins/kirby-calendars/src/index.js
+++ cms/site/plugins/kirby-calendars/src/index.js
@@ -1,11 +1,13 @@
 import SchedulesView from "./components/SchedulesView.vue";
 import ServicesView from "./components/ServicesView.vue";
 import CalendarsView from "./components/CalendarsView.vue";
+import EventsView from "./components/EventsView.vue";
 
 panel.plugin("mediumsans/kirby-calendars", {
   components: {
     'k-calendars-view': CalendarsView,
     'k-schedules-view': SchedulesView,
-    'k-services-view': ServicesView
+    'k-services-view': ServicesView,
+    'k-events-view': EventsView
   }
 });
