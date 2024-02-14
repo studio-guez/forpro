af1f90d398a2ac6e5921b4c43a89f88f7604bcdd
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 16:59:16 +0100

    chore: refactor the urls to gather info from the cms

--- website/src/lib/utils/booking/urls.ts
+++ website/src/lib/utils/booking/urls.ts
@@ -1,13 +1,17 @@
 import {variables} from "$lib/utils/constants";
 
-export const getAvailabilitiesUrl = (providerId: string, servicedId: string, date: string): string => {
-    return `${variables.BOOKING_BASE_URL}/agenda/day/${providerId}/${servicedId}/${date}`;
+export const getSlots = (calendarId: string, servicedId: string, date: string): string => {
+    const url = `${variables.CMS_BASE_URL}/kirby-calendars/get-slots/${calendarId}/${servicedId}/${date}`;
+    console.log(url);
+    return url;
 }
 
-export const getServicesFromProviderUrl = (providerId: string): string => {
-    return `${variables.BOOKING_BASE_URL}/provider/${providerId}`;
+export const getServices = (calendarId: string): string => {
+    const url = `${variables.CMS_BASE_URL}/kirby-calendars/${calendarId}/services/`;
+    console.log(url);
+    return url;
 }
 
 export const getAppointmentsUrl = (): string => {
-    return `${variables.BOOKING_BASE_URL}/api/appointment/`;
+    return `${variables.CMS_BASE_URL}/kirby-calendars/api/appointment/`;
 }
