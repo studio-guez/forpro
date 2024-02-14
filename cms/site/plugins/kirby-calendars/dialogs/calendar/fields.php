a6b6497ff2ba992125f79126c76dffec42a18aac
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 21:13:09 +0100

    chore: clean easy creation and form validation of calendars

--- cms/site/plugins/kirby-calendars/dialogs/calendar/fields.php
+++ cms/site/plugins/kirby-calendars/dialogs/calendar/fields.php
@@ -5,13 +5,4 @@ return [
         'label' => 'Nom',
         'type' => 'text'
     ],
-    'description' => [
-        'label' => 'Description',
-        'type' => 'textarea',
-        'buttons' => false
-    ],
-    'location' => [
-        'label' => 'Localisation',
-        'type' => 'text'
-    ],
 ];
