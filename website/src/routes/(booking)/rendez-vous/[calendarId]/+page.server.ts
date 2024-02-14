17cc154691bbb4738864310337e012e5ac0a6153
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-14 19:37:10 +0100

    fix: refactor default actions to submit form data

--- website/src/routes/(booking)/rendez-vous/[calendarId]/+page.server.ts
+++ website/src/routes/(booking)/rendez-vous/[calendarId]/+page.server.ts
@@ -1,16 +1,12 @@
 import type {Actions, PageServerLoad} from './$types';
 import {variables} from "$lib/utils/constants";
 import type {BookingCMSResponse} from "$lib/interfaces/variables";
-import {
-    createAppointment,
-    getAvailableSlots,
-    getServicesFromCalendarId,
-} from "$lib/utils/booking/api";
+import {createAppointment, getAvailableSlots, getServicesFromCalendarId,} from "$lib/utils/booking/api";
 import dayjs from "dayjs";
 
 export const prerender = false;
 
-export const load: PageServerLoad = async ({ params, fetch }) => {
+export const load: PageServerLoad = async ({params, fetch}) => {
     const cmsBookingUrl = `${variables.CMS_BASE_URL}/booking`
     const calendarId = params.calendarId;
 
@@ -22,56 +18,48 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
     const servicesKirby = await getServicesFromCalendarId(calendarId);
     const services = Object.values(servicesKirby);
 
-    console.log(services);
-
     const today = dayjs().format('YYYY-MM-DD');
 
     if (services !== undefined) {
         availabilities = await getAvailableSlots(today, services[0].id, calendarId);
     }
 
-    console.log(availabilities);
-
-    return { content, services, calendarId, availabilities };
+    return {content, services, calendarId, availabilities};
 };
 
 export const actions = {
-    default: async ({ request }) => {
-        const { email, firstname, lastname, phone, providerId, serviceId, notes, dateAndSlotAndDuration } = await getFormData(request);
-        const { start, end } = createDates(dateAndSlotAndDuration);
+    default: async ({request}) => {
 
-        const appointment = await createAppointment(
-            start,
-            end,
-            serviceId,
-            providerId,
-            notes,
+        const data = await request.formData();
+        const email = data.get('email');
+        const firstname = data.get('firstname');
+        const lastname = data.get('lastname');
+        const phone = data.get('phone');
+        const calendarId = data.get('calendarId');
+        const serviceId = data.get('serviceId');
+        const date = data.get('date');
+        const slot = data.get('slot');
+
+        const infos = {
             firstname,
             lastname,
             phone,
-            email
+            email,
+        };
+
+        const appointment = await createAppointment(
+            date,
+            slot,
+            serviceId,
+            calendarId,
+            infos
         );
 
-        console.log(appointment);
-
-        return { appointment };
+        return {appointment};
     },
 } satisfies Actions;
 
-const getFormData = async (request) => {
-    const data            = await request.formData();
-    const email           = data.get('email');
-    const firstname       = data.get('firstname');
-    const lastname        = data.get('lastname');
-    const phone           = data.get('phone');
-    const providerId      = data.get('providerId');
-    const serviceId       = data.get('serviceId');
-    const notes           = data.get('notes');
-    const dateAndSlotAndDuration     = { date: data.get('date'), slot: data.get('slot'), duration: data.get('duration') };
-    return { email, firstname, lastname, phone, providerId, serviceId, notes, dateAndSlotAndDuration };
-}
-
-const createDates = ({ date, slot, duration }) => {
+const createDates = ({date, slot, duration}) => {
     const start = dayjs(`${date}T${slot}:00`);
     console.log(start);
 
@@ -82,5 +70,5 @@ const createDates = ({ date, slot, duration }) => {
     const formattedStart = start.format('YYYY-MM-DD HH:mm:ss');
     const formattedEnd = end.format('YYYY-MM-DD HH:mm:ss');
 
-    return { start: formattedStart, end: formattedEnd };
+    return {start: formattedStart, end: formattedEnd};
 };
