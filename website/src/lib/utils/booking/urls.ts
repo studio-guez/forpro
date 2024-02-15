305ac41b7b67a0a597ae180e70fd39f27b54570c
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-15 13:40:20 +0100

    feat: add getServices url helper

--- website/src/lib/utils/booking/urls.ts
+++ website/src/lib/utils/booking/urls.ts
@@ -1,15 +1,15 @@
 import {variables} from "$lib/utils/constants";
 
 export const getSlots = (calendarId: string, servicedId: string, date: string): string => {
-    const url = `${variables.CMS_BASE_URL}/kirby-calendars/get-slots/${calendarId}/${servicedId}/${date}`;
-    console.log(url);
-    return url;
+    return `${variables.CMS_BASE_URL}/kirby-calendars/get-slots/${calendarId}/${servicedId}/${date}`;
 }
 
 export const getServices = (calendarId: string): string => {
-    const url = `${variables.CMS_BASE_URL}/kirby-calendars/${calendarId}/services/`;
-    console.log(url);
-    return url;
+    return `${variables.CMS_BASE_URL}/kirby-calendars/${calendarId}/services/`;
+}
+
+export const getSchedules = (calendarId: string): string => {
+    return `${variables.CMS_BASE_URL}/kirby-calendars/${calendarId}/schedules/`;
 }
 
 export const getAppointmentsUrl = (): string => {
