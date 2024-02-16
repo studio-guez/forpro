7135f3c745a06bc29ec9846a3c639e64aee2febd
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-16 15:04:37 +0100

    chore: migrate methods to the new notification flow

---
+++ cms/site/plugins/kirby-calendars/classes/Utils.php
@@ -0,0 +1,36 @@
+<?php
+
+Namespace MediumSans\KirbyCalendars;
+
+use Google\Exception;
+use Google_Client;
+use Google_Service_Calendar;
+
+class Utils
+{
+    /**
+     * Converts a duration string in the format "HH:MM" to minutes
+     *
+     * @param string $duration The duration string in the format "HH:MM"
+     * @return int The duration in minutes
+     */
+    public static function convertDurationToMinutes(string $duration): int
+    {
+        [$hours, $minutes] = explode(':', $duration);
+        return $hours * 60 + $minutes;
+    }
+
+    /**
+     * Creates a new instance of Google_Service_Calendar using Google_Client
+     *
+     * @return Google_Service_Calendar
+     * @throws Exception
+     */
+    public static function createGoogleService(): Google_Service_Calendar
+    {
+        $client = new Google_Client();
+        $client->setAuthConfig(__DIR__ . '/../../../config/forpro-calendars-9f6da95c3aa9.json');
+        $client->addScope(Google_Service_Calendar::CALENDAR);
+        return new Google_Service_Calendar($client);
+    }
+}
