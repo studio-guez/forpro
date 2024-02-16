425555daf292ad3cc56703460e84b3f15d9f86a4
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-16 15:54:49 +0100

    chore: rename and optimize function

--- cms/site/plugins/kirby-calendars/classes/Mail.php
+++ cms/site/plugins/kirby-calendars/classes/Mail.php
@@ -10,7 +10,7 @@ class Mail
     /**
      * @throws Exception
      */
-    public static function sendConfirmAppointmentNotification(
+    public static function sendPleaseConfirmEventNotification(
         string $email,
         string $eventId,
         string $serviceName,
@@ -22,8 +22,9 @@ class Mail
     {
         $kirby = kirby();
 
-        $name = 'Forpro - Confirmation de rendez-vous';
-        $from = static::createFrom($email, $name);
+        $f_email = $kirby->option('mediumsans.kirbycalendars.notifications.from');
+        $f_name = $kirby->option('mediumsans.kirbycalendars.notifications.event_confirmation.name');
+        $from = static::createFrom($f_email, $f_name);
 
         try {
             $notified = $kirby->email([
@@ -48,34 +49,38 @@ class Mail
         return $notified;
     }
 
-    public static function sendAppointmentConfirmationToPersonInCharge(
-        string $email,
-        string $serviceName,
-        string $firstname,
-        string $lastname,
+    /**
+     * @throws Exception
+     */
+    public static function sendEventIsConfirmedToPersonInCharge(
+        array  $calendar,
+        array  $event,
+        array  $service,
         string $startDate,
         string $startTime
     ): bool
     {
-        $name = 'Forpro - RDV confirmé';
-        $from = static::createFrom($email, $name);
+        $kirby = kirby();
+
+        $f_email = $kirby->option('mediumsans.kirbycalendars.notifications.from');
+        $f_name = $kirby->option('mediumsans.kirbycalendars.notifications.calendar_incharge.name');
+        $from = static::createFrom($f_email, $f_name);
 
         try {
             $notified = $kirby->email([
                 'from' => $from,
-                'to' => $email,
+                'to' => $calendar['email'],
                 'subject' => 'Nouveau RDV !',
                 'template' => 'calendar_incharge',
                 'data' => [
-                    'firstname' => $firstname,
-                    'lastname' => $lastname,
-                    'service' => $eventId,
-                    'email' => $email,
-                    'phone' => $phone,
-                    'serviceName' => $serviceName,
+                    'firstname' => $event['firstname'],
+                    'description' => $event['description'],
+                    'lastname' => $event['lastname'],
+                    'email' => $event['email'],
+                    'phone' => $event['phone'],
+                    'serviceName' => $service['name'],
                     'startDate' => $startDate,
                     'startTime' => $startTime,
-                    'validationURL' => 'https://forpro-website.sdrvl.ch/confirm/' . $eventId,
                 ],
             ])->isSent();
         } catch (Exception $error) {
@@ -87,11 +92,9 @@ class Mail
 
     private static function createFrom($email, $name): User
     {
-        $from = new User([
+        return new User([
             'email' => $email,
             'name' => $name,
         ]);
-
-        return $from;
     }
 }
