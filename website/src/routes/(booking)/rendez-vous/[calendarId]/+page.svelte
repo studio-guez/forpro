d692105b73884cabdfeeff6664a7282f53131264
azertypow <azertypow@gmail.com>
2024-02-19 21:30:44 +0100

    feat: last step calendar text

--- website/src/routes/(booking)/rendez-vous/[calendarId]/+page.svelte
+++ website/src/routes/(booking)/rendez-vous/[calendarId]/+page.svelte
@@ -361,14 +361,17 @@
                                     </div>
                                 {:else}
 
-                                    <div class="mt-12 h-full w-full my-auto mx-auto">
-                                        <div class="w-12 h-12 rounded-full bg-green-100 dark:bg-green-900 p-2 flex items-center justify-center mx-auto mb-3.5">
-                                            <svg aria-hidden="true" class="w-8 h-8 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                            <span class="sr-only">Success</span>
-                                        </div>
-                                        <p class="mb-4 text-center text-lg font-semibold text-black max-w-2xl">
-                                            {data.content.bookingAppointmentSuccessLabel}
-                                        </p>
+                                    <div class="app-page-simple_content" >
+                                        <h1 style="color: var(--app-color--green)">
+                                            Merci&nbsp;!
+                                        </h1>
+                                        <h5 style="color: var(--app-color--blue)">
+                                            Encore une étape…
+                                        </h5>
+                                        <p>{data.content.bookingAppointmentSuccessLabel}</p>
+                                        <a class="app-button app-button--rounded"
+                                           href="/"
+                                        >Retourner à la page d'accueil</a>
                                     </div>
                                 {/if}
                             </div>
