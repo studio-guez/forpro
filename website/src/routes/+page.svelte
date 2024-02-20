de36b5f79b5bd56993550333f169d9571f419ddb
azertypow <azertypow@gmail.com>
2024-02-20 02:03:03 +0100

    feat: animation home header

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -2,11 +2,22 @@
 
 >
     <section class="s-page-home__header app-flex app-flex--column app-flex--justify_center app-flex--align_center app-flex--nowrap">
-        <img class="s-page-home__header__logo"
-                src="/logo.svg" alt="logo">
-        <img class="s-page-home__header__logo"
-             src="/logo.svg" alt="logo">
-
+        {#if browser}
+        <LottiePlayer
+                src="lottie/desktop_logo-header.json"
+                autoplay="{true}"
+                background="transparent"
+                speed="1"
+                style="width: 100%; height: 100%"
+                direction="1"
+                mode="normal"
+                width="100%"
+                height="100%"
+                controls="{false}"
+                controlsLayout="[]"
+                renderer="svg"
+        />
+        {/if}
     </section>
     <AppPage
             data="{data}"
@@ -17,6 +28,8 @@
 <script lang="ts">
     import {type IPage} from "$lib/interfaces/cmsApiResponse";
     import AppPage from "$lib/components/AppPage.svelte";
+    import {LottiePlayer} from "@lottiefiles/svelte-lottie-player";
+    import {browser} from "$app/environment";
 
     export let data: IPage;
 
