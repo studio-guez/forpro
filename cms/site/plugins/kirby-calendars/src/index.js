08ec3306d30bce91e39b97432b8549733eafff99
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-24 12:33:57 +0100

    feat: Add Leaves hooks

--- cms/site/plugins/kirby-calendars/src/index.js
+++ cms/site/plugins/kirby-calendars/src/index.js
@@ -2,12 +2,14 @@ import SchedulesView from "./components/SchedulesView.vue";
 import ServicesView from "./components/ServicesView.vue";
 import CalendarsView from "./components/CalendarsView.vue";
 import EventsView from "./components/EventsView.vue";
+import LeavesView from "./components/LeavesView.vue";
 
 panel.plugin("mediumsans/kirby-calendars", {
   components: {
     'k-calendars-view': CalendarsView,
     'k-schedules-view': SchedulesView,
     'k-services-view': ServicesView,
-    'k-events-view': EventsView
+    'k-events-view': EventsView,
+    'k-leaves-view': LeavesView
   }
 });
