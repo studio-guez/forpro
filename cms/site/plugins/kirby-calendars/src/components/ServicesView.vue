2bc14360c335322c05a72e103fc60ab92aa91a58
Samuel Cardoso <samuel@mediumsans.studio>
2024-02-24 13:47:59 +0100

    fix: typo

--- cms/site/plugins/kirby-calendars/src/components/ServicesView.vue
+++ cms/site/plugins/kirby-calendars/src/components/ServicesView.vue
@@ -1,7 +1,7 @@
 <template>
   <k-inside>
     <k-header>
-      { $t('kirbycalendars.services') }}
+      {{ $t('kirbycalendars.services') }}
 
       <k-button-group slot="buttons">
         <k-button
