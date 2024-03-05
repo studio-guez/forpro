da36a8db3aab575ff02ef4ffa39f73fb63e726ef
azertypow <azertypow@gmail.com>
2024-03-05 14:48:39 +0100

    feat: mobile home header

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -3,20 +3,40 @@
 >
     <section class="s-page-home__header app-flex app-flex--column app-flex--justify_center app-flex--align_center app-flex--nowrap">
         {#if browser}
-        <LottiePlayer
-                src="lottie/desktop_logo-header.json"
-                autoplay="{true}"
-                background="transparent"
-                speed="1"
-                style="width: 100%; height: 100%"
-                direction="1"
-                mode="normal"
-                width="100%"
-                height="100%"
-                controls="{false}"
-                controlsLayout="[]"
-                renderer="svg"
-        />
+            <div class="s-page-home__header__logo__desktop">
+
+                <LottiePlayer
+                        src="lottie/desktop_logo-header.json"
+                        autoplay="{true}"
+                        background="transparent"
+                        speed="1"
+                        style="width: 100%; height: 100%"
+                        direction="1"
+                        mode="normal"
+                        width="100%"
+                        height="100%"
+                        controls="{false}"
+                        controlsLayout="[]"
+                        renderer="svg"
+                />
+            </div>
+            <div class="s-page-home__header__logo__mobile">
+                <LottiePlayer
+                        src="lottie/mobile_logo-header.json"
+                        autoplay="{true}"
+                        background="transparent"
+                        speed="1"
+                        style="width: 100%; height: 100%"
+                        direction="1"
+                        mode="normal"
+                        width="100%"
+                        height="100%"
+                        controls="{false}"
+                        controlsLayout="[]"
+                        renderer="svg"
+                />
+            </div>
+
         {/if}
     </section>
     <AppPage
@@ -43,7 +63,22 @@
   height: max(80vh, 40vw);
 
   @media (max-width: scss-params.$fp-breakpoint-xs) {
-    //height: auto;
+    height: auto;
+  }
+}
+
+.s-page-home__header__logo__desktop {
+  display: block;
+
+  @media (max-width: scss-params.$fp-breakpoint-xs) {
+    display: none;
+  }
+}
+.s-page-home__header__logo__mobile {
+  display: none;
+
+  @media (max-width: scss-params.$fp-breakpoint-xs) {
+    display: block;
   }
 }
 
