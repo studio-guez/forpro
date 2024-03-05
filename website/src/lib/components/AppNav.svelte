c14f2e3d7265ae067615bf51af715cfde6dfd10a
azertypow <azertypow@gmail.com>
2024-03-05 16:20:31 +0100

    feat: desktop nav typo

--- website/src/lib/components/AppNav.svelte
+++ website/src/lib/components/AppNav.svelte
@@ -148,11 +148,14 @@
     }
 
     .s-app-nav__list__item {
+      line-height: 1em;
+
       @media (max-width: scss-params.$fp-breakpoint-sm) {
         margin-bottom: .25em;
       }
 
       &:not(.is-subpage) {
+        margin-top: .35em;
           @media (max-width: scss-params.$fp-breakpoint-sm) {
             margin-top: 1em;
           }
