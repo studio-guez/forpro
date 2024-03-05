a81d78d2e96406c6cb8620dc5ef828d93dfd0972
azertypow <azertypow@gmail.com>
2024-03-05 14:49:15 +0100

    feat: clean code

--- website/src/lib/components/AppPage.svelte
+++ website/src/lib/components/AppPage.svelte
@@ -62,7 +62,7 @@
 
             {:else if (content.type === 'map' && content.content.style === "style1")}
                 <div class="app-flex__basis-20-24 app-flex app-flex--justify_center">
-                    <BlockSpaceBuilding content="{content}" />
+                    <BlockSpaceBuilding />
                 </div>
             {:else if (content.type === 'map')}
                 <div class="app-flex__basis-20-24 app-flex app-flex--justify_center">
