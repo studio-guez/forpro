408eb8ddf52432ad47c6a495925d983b12c17f2e
azertypow <azertypow@gmail.com>
2024-04-25 13:15:44 +0200

    feat: add gradient color system

--- cms/site/templates/home.json.php
+++ cms/site/templates/home.json.php
@@ -40,6 +40,7 @@ $json['website'] = [
 
 $json['options'] = [
     'showMenu' => $showMenu,
+    'gradientColor' => $hero->gradientColor()->value(),
     'showNewsletter' => $showNewsletter,
     'hero' => $hero ? [
         'text' => $hero->text()->value(),
