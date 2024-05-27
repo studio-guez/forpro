0d40059c81f7a4dd2bf999d81393cda1c2ae0041
azertypow <azertypow@gmail.com>
2024-05-27 10:49:44 +0200

    feat: retour

--- website/src/routes/changerderegard/+page.svelte
+++ website/src/routes/changerderegard/+page.svelte
@@ -6,17 +6,28 @@
       <h1><span class="s-page-changer-de-regard__title__big">10 raisons</span>
           <br>de changer de point de vue sur l’apprentissage</h1>
       <ul class="app-list-fp-style">
-        <li>Le système d'apprentissage en Suisse est hautement valorisé pour son modèle dual. Unique, il combine enseignement théorique en école professionnelle et expérience pratique en entreprise. Il répond aux exigences actuelles des entreprises et renforce l'employabilité des apprenti·e·s sur le marché du travail&nbsp;: c’est un véritable passeport pour l’emploi.</li>
+        <li>Le système d'apprentissage en Suisse est hautement valorisé pour son modèle dual. Unique, il combine enseignement théorique en école professionnelle et expérience pratique en entreprise. Il répond aux exigences actuelles des entreprises et renforce l'employabilité des apprenti·e·s sur le marché du travail: c’est un véritable passeport pour l’emploi.</li>
         <li>Avec plus de 240 formations disponibles, l'apprentissage couvre tous les secteurs d'activité, offrant une diversité d’options adaptées à différentes passions et intérêts.</li>
         <li>Tout sauf une impasse, l'apprentissage permet d’accéder à l'enseignement supérieur via la maturité professionnelle, permettant la poursuite d'études dans les hautes écoles spécialisées, les universités et les écoles polytechniques fédérales.</li>
         <li>L'apprentissage permet aux jeunes de bénéficier d'une rémunération dès le début de leur formation, avec des perspectives salariales souvent plus favorables que celles offertes par la formation générale.</li>
-        <li>La formation professionnelle permet d’obtenir un diplôme donnant accès au marché du travail sans nécessiter de longues années d'études. Un CFC, c’est trois ans d’études et un métier en main&nbsp;! Et si on choisit de poursuivre ses études, à chaque étape, on peut opter pour l’emploi.</li>
+        <li>La formation professionnelle permet d’obtenir un diplôme donnant accès au marché du travail sans nécessiter de longues années d'études. Un CFC, c’est trois ans d’études et un métier en main! Et si on choisit de poursuivre ses études, à chaque étape, on peut opter pour l’emploi.</li>
         <li>L'apprentissage offre aux jeunes la possibilité de transformer leur passion en carrière professionnelle, leur permettant d'exercer un métier qu'ils aiment. De plus, celles et ceux qui ont opté pour cette voie après le Cycle d'orientation expriment une satisfaction sensiblement supérieure à celle des jeunes ayant choisi une filière généraliste.</li>
-        <li>L'apprentissage permet aux apprenti∙e∙s d'appliquer immédiatement les connaissances acquises et d'évoluer rapidement dans leur domaine.</li>
+        <li>L'apprentissage permet aux apprenti·e·s d'appliquer immédiatement les connaissances acquises et d'évoluer rapidement dans leur domaine.</li>
         <li>Le choix d’un apprentissage après le Cycle d’orientation n’entrave en rien la possibilité d’une réorientation professionnelle par la suite et permet une mobilité professionnelle aussi grande qu’avec un diplôme d’une haute école.</li>
         <li>Créer sa propre entreprise devient une option concrète après l'obtention d'une AFP ou d'un CFC. L'entrepreneuriat est facilité par les années d’expérience pratique accumulées durant la formation.</li>
         <li>Le système d'apprentissage suisse est reconnu et envié mondialement, et peut ouvrir les portes à des opportunités de carrière à l’étranger.</li>
       </ul>
+
+      <div class="s-page-changer-de-regard__cta-desktop">
+        <a class="app-button app-button--rounded"
+           href="/"
+           style="
+                --app-button--color: black;
+                --app-button--background-color: var(--app-color--pink);
+                --app-button--border-color: var(--app-color--pink);
+              "
+        >En savoir +</a>
+      </div>
     </div>
   </div>
   <div class="s-page-changer-de-regard__footer">
@@ -104,6 +115,10 @@
       position: sticky;
       top: calc( -1 * var(--app-nav_height));
       flex-shrink: 0;
+
+      @media (min-width: $page-change-de-regard--breakpoint) {
+        top: 0;
+      }
     }
 
     &:before {
@@ -126,6 +141,22 @@
     }
   }
 
+  .s-page-changer-de-regard__cta-desktop {
+    display: none;
+
+    @media (min-width: $page-change-de-regard--breakpoint) {
+      display: flex;
+      justify-content: center;
+      padding-top: 2rem;
+      padding-bottom: 2rem;
+    }
+
+    img {
+      display: block;
+      height: 2rem;
+    }
+  }
+
   .s-page-changer-de-regard__footer {
     position: sticky;
     bottom: 0;
@@ -137,5 +168,9 @@
     justify-content: center;
     padding-top: .5rem;
     padding-bottom: .5rem;
+
+    @media (min-width: $page-change-de-regard--breakpoint) {
+      display: none;
+    }
   }
 </style>
