ed942ea6c5c004b6b49565e017f1d0dedd8355c0
Samuel Cardoso <samuel@mediumsans.studio>
2024-06-18 15:55:45 +0200

    chore: bis

--- restaurant/svelte.config.js
+++ restaurant/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,7 +15,7 @@ const config = {
 		paths: {
 			base: ''
 		}
-	},
+	}
 };
 
 export default config;
