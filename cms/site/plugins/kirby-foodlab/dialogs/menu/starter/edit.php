446b5be15b299c9ed301e03d007bb093e7b2bd7c
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:24:28 +0200

    feat: add starter dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/starter/edit.php
@@ -0,0 +1,21 @@
+<?php
+
+use MediumSans\Menu\Starter;
+
+return [
+    'pattern' => 'menu/starter/(:any)/edit',
+    'load'    => function (string $id) {
+        $starter = Starter::find($id);
+
+        return [
+            'component' => 'k-form-dialog',
+            'props' => [
+                'fields' => require __DIR__ . '/fields.php',
+                'value' => $starter
+            ]
+        ];
+    },
+    'submit' => function (string $id) {
+        return Starter::update($id, get());
+    }
+];
