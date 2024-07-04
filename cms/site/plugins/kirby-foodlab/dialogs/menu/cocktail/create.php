5df12866e66b2c7d2ec5be15c2b9976df1dca258
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:23:26 +0200

    feat: add Cocktail dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/cocktail/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use MediumSans\Menu\Cocktail;
+
+return [
+    'pattern' => 'menu/cocktail/create',
+    'load'    => function () {
+        return [
+            'component' => 'k-form-dialog',
+            'props'     => [
+                'fields'        => require __DIR__ . '/fields.php',
+                'submitButton'  => t('create'),
+                'size'          => 'large',
+            ],
+        ];
+    },
+    'submit' => function () {
+        return Cocktail::create(get());
+    }
+];
