34625d33d2fda28cca10e6c96f2ba8dd5e90d037
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:21:18 +0200

    feat: add BubbleWine class

---
+++ cms/site/plugins/kirby-foodlab/classes/BubbleWine.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MediumSans\Menu;
+
+use Kirby\Data\Data;
+use MediumSans\BaseClass;
+
+class BubbleWine extends BaseClass {
+
+    const FILENAME = 'bubblewine.json';
+
+    /**
+     * Creates a new menu with the given $input
+     * data and adds it to the json file
+     *
+     * @param array $input
+     * @return bool
+     */
+    public static function create(array $input): bool
+    {
+        $id = uuid();
+
+        $bubbleWine = [
+            "id"            => $id,
+            "name"          => $input["name"] ?? "",
+            "description"   => $input["description"],
+            "domain"        => $input["domain"],
+            "mill"          => $input["mill"],
+            "price10cl"     => $input["price10cl"],
+            "price75cl"     => $input["price75cl"],
+        ];
+
+        $bubbleWines = self::list();
+        $bubbleWines[] = $bubbleWine;
+
+        return Data::write(static::file(), $bubbleWines);
+    }
+
+    /**
+     * Updates a menu by id with the given input
+     * It throws an exception in case of validation issues
+     *
+     * @param array $menu
+     * @return boolean
+     */
+    public static function update(array $menu): bool
+    {
+        return Data::write(static::file(), $menu);
+    }
+}
