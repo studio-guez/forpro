f2261e4b4bbc26c658e2674889277ceb8f767f4b
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:23:16 +0200

    feat: add BubbleWine dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/bubblewine/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use MediumSans\Menu\BubbleWine;
+
+return [
+    'pattern' => 'menu/bubblewine/create',
+    'load'    => function () {
+        return [
+            'component' => 'k-form-dialog',
+            'props'     => [
+                'fields'        => require __DIR__ . '/fields.php',
+                'submitButton'  => t('create'),
+                'size'          => 'large',
+            ],
+        ];
+    },
+    'submit' => function () {
+        return BubbleWine::create(get());
+    }
+];
