d0bbf98e75901ec7437e72577529e4b5f09da547
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:24:20 +0200

    feat: add softdrinks dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/softdrink/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use MediumSans\Menu\Softdrink;
+
+return [
+    'pattern' => 'menu/softdrink/create',
+    'load'    => function () {
+        return [
+            'component' => 'k-form-dialog',
+            'props'     => [
+                'fields'        => require __DIR__ . '/fields.php',
+                'submitButton'  => t('create'),
+                'size'          => 'large',
+            ],
+        ];
+    },
+    'submit' => function () {
+        return Softdrink::create(get());
+    }
+];
