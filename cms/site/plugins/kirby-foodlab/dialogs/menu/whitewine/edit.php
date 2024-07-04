f9d59fd67a07ba7052155e079519029bcf24a825
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:24:36 +0200

    feat: add whitewine dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/whitewine/edit.php
@@ -0,0 +1,21 @@
+<?php
+
+use MediumSans\Menu\Whitewine;
+
+return [
+    'pattern' => 'menu/whitewine/(:any)/edit',
+    'load'    => function (string $id) {
+        $whitewine = Whitewine::find($id);
+
+        return [
+            'component' => 'k-form-dialog',
+            'props' => [
+                'fields' => require __DIR__ . '/fields.php',
+                'value' => $whitewine
+            ]
+        ];
+    },
+    'submit' => function (string $id) {
+        return Whitewine::update($id, get());
+    }
+];
