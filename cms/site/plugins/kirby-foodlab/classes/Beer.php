e12bc40c82e392a1ced3cf2c2c7278fed91d93c5
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 18:34:47 +0200

    fix: add the possibility for the data to be incomplete

--- cms/site/plugins/kirby-foodlab/classes/Beer.php
+++ cms/site/plugins/kirby-foodlab/classes/Beer.php
@@ -23,9 +23,9 @@ class Beer extends BaseClass {
         $hotdrink = [
             "id"            => $id,
             "name"          => $input["name"] ?? "",
-            "description"   => $input["description"],
-            "volume"        => $input["volume"],
-            "price"         => $input["price"],
+            "description"   => $input["description"] ?? "",
+            "volume"        => $input["volume"] ?? "",
+            "price"         => $input["price"] ?? "",
         ];
 
         $hotdrinks = self::list();
