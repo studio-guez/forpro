4c2264be8e6eef7c46cd1a31b61e01043eb23fa0
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:23:35 +0200

    feat: add Dessert dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/dessert/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use MediumSans\Menu\Dessert;
+
+return [
+    'pattern' => 'menu/dessert/create',
+    'load'    => function () {
+        return [
+            'component' => 'k-form-dialog',
+            'props'     => [
+                'fields'        => require __DIR__ . '/fields.php',
+                'submitButton'  => t('create'),
+                'size'          => 'large',
+            ],
+        ];
+    },
+    'submit' => function () {
+        return Dessert::create(get());
+    }
+];
