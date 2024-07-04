f33b0869a91a4d13e170c10111e739fc71c69729
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:25:18 +0200

    feat: add Restaurant View

---
+++ cms/site/plugins/kirby-foodlab/src/components/RestaurantView.vue
@@ -0,0 +1,38 @@
+<template>
+  <k-inside>
+    <k-header>
+      Restaurant
+    </k-header>
+
+
+  </k-inside>
+</template>
+
+<script>
+export default {
+  props: {},
+  methods: {
+    goto(path) {
+      this.$go(path);
+    },
+    shortenUrl(url) {
+      if (url.length > 25) return url.slice(0, 22) + '...';
+      else return url;
+    },
+    async copyToClipboard(text) {
+      try {
+        await navigator.clipboard.writeText(text);
+        window.panel.notification.success("URL du calendrier copiée dans le presse-papier");
+      } catch (err) {
+        // Show error message
+      }
+    },
+  }
+};
+</script>
+
+<style>
+.k-table.k-calendar {
+  table-layout: fixed;
+}
+</style>
