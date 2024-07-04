33aec531f9327d679308378cdb150d7db395e57a
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:24:36 +0200

    feat: add whitewine dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/whitewine/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use MediumSans\Menu\Whitewine;
+
+return [
+    'pattern' => 'menu/whitewine/create',
+    'load'    => function () {
+        return [
+            'component' => 'k-form-dialog',
+            'props'     => [
+                'fields'        => require __DIR__ . '/fields.php',
+                'submitButton'  => t('create'),
+                'size'          => 'large',
+            ],
+        ];
+    },
+    'submit' => function () {
+        return Whitewine::create(get());
+    }
+];
---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu/whitewine/delete.php
@@ -0,0 +1,18 @@
+<?php
+
+use MediumSans\Menu\Whitewine;
+
+return [
+    'pattern' => 'menu/whitewine/(:any)/delete',
+    'load' => function () {
+        return [
+            'component' => 'k-remove-dialog',
+            'props' => [
+                'text' => 'Êtes-vous sûr de vouloir supprimer cet élément ?',
+            ]
+        ];
+    },
+    'submit' => function (string $id) {
+        return Whitewine::delete($id);
+    }
+];
