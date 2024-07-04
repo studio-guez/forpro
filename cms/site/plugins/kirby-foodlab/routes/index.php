4dd37be2bd3f5455496bdaceb9c0118a7170acaf
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-04 16:25:02 +0200

    feat: add routes

---
+++ cms/site/plugins/kirby-foodlab/routes/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use Kirby\Cms\Page;
+use Kirby\Cms\Response;
+use MediumSans\Menu;
+use Mpdf\Mpdf;
+use Nuzkito\ChromePdf\ChromePdf;
+use Spatie\Browsershot\Browsershot;
+
+return [
+    'routes' => function () {
+        return [
+            /* Menu */
+            [
+                'pattern' => 'restaurant/menu/create',
+                'method' => 'POST',
+                'action' => function () {
+                    return Menu::create(get());
+                }
+            ],
+            [
+                'pattern' => 'restaurant/menu/generate',
+                'method' => 'GET',
+                'auth' => false,
+                'action' => function () {
+                    kirby()->impersonate('kirby');
+
+                    $data = Menu::get();
+
+                    $menu_page = Page::factory([
+                        'slug' => 'menu',
+                        'template' => 'menu-pdf',
+                        'model' => 'menu-pdf',
+                        'content' => $data
+                    ]);
+
+                    $html = $menu_page->render($data);
+                    $pdfContent = Browsershot::html($html)->showBackground()->pdf();
+
+                    return new Response($pdfContent, 'application/pdf', 200, [
+                        'Content-Disposition' => 'attachment; filename="menu.pdf"'
+                    ]);
+                }
+            ],
+        ];
+    }
+];
