b1feb8f2967cb217de21c9d126409a38803c82cb
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-05 14:45:41 +0200

    feat: add new create route for restaurant

--- cms/site/plugins/kirby-foodlab/routes/index.php
+++ cms/site/plugins/kirby-foodlab/routes/index.php
@@ -3,45 +3,57 @@
 use Kirby\Cms\Page;
 use Kirby\Cms\Response;
 use MediumSans\Menu;
+use MediumSans\Restaurant;
 use Mpdf\Mpdf;
 use Nuzkito\ChromePdf\ChromePdf;
 use Spatie\Browsershot\Browsershot;
 
 return [
-    'routes' => function () {
+    "routes" => function () {
         return [
             /* Menu */
             [
-                'pattern' => 'restaurant/menu/create',
-                'method' => 'POST',
-                'action' => function () {
+                "pattern" => "restaurant/menu/create",
+                "method" => "POST",
+                "action" => function () {
                     return Menu::create(get());
-                }
+                },
             ],
             [
-                'pattern' => 'restaurant/menu/generate',
-                'method' => 'GET',
-                'auth' => false,
-                'action' => function () {
-                    kirby()->impersonate('kirby');
+                "pattern" => "restaurant/menu/generate",
+                "method" => "GET",
+                "auth" => false,
+                "action" => function () {
+                    kirby()->impersonate("kirby");
 
                     $data = Menu::get();
 
                     $menu_page = Page::factory([
-                        'slug' => 'menu',
-                        'template' => 'menu-pdf',
-                        'model' => 'menu-pdf',
-                        'content' => $data
+                        "slug" => "menu",
+                        "template" => "menu-pdf",
+                        "model" => "menu-pdf",
+                        "content" => $data,
                     ]);
 
                     $html = $menu_page->render($data);
-                    $pdfContent = Browsershot::html($html)->format('A4')->showBackground()->pdf();
+                    $pdfContent = Browsershot::html($html)
+                        ->format("A4")
+                        ->showBackground()
+                        ->pdf();
 
-                    return new Response($pdfContent, 'application/pdf', 200, [
-                        'Content-Disposition' => 'attachment; filename="menu.pdf"'
+                    return new Response($pdfContent, "application/pdf", 200, [
+                        "Content-Disposition" =>
+                            'attachment; filename="menu.pdf"',
                     ]);
-                }
+                },
+            ],
+            [
+                "pattern" => "restaurant/update",
+                "method" => "POST",
+                "action" => function () {
+                    return Restaurant::create(get());
+                },
             ],
         ];
-    }
+    },
 ];
