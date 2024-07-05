b8c09a5d3c8ff881a13b2e512567a5a50c62c3dd
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-05 15:26:45 +0200

    chore: add tabs/restaurant blueprint

--- cms/site/plugins/kirby-foodlab/index.php
+++ cms/site/plugins/kirby-foodlab/index.php
@@ -1,104 +1,93 @@
 <?php
 
-@include_once __DIR__ . '/vendor/autoload.php';
+@include_once __DIR__ . "/vendor/autoload.php";
 
 load([
-    'MediumSans\BaseClass'       => __DIR__ . '/classes/BaseClass.php',
-    'MediumSans\Menu'            => __DIR__ . '/classes/Menu.php',
-    'MediumSans\Restaurant'      => __DIR__ . '/classes/Restaurant.php',
-    'MediumSans\Menu\MainCourse' => __DIR__ . '/classes/MainCourse.php',
-    'MediumSans\Menu\Starter'    => __DIR__ . '/classes/Starter.php',
-    'MediumSans\Menu\Dessert'    => __DIR__ . '/classes/Dessert.php',
-    'MediumSans\Menu\RedWine'    => __DIR__ . '/classes/RedWine.php',
-    'MediumSans\Menu\WhiteWine'  => __DIR__ . '/classes/WhiteWine.php',
-    'MediumSans\Menu\BubbleWine' => __DIR__ . '/classes/BubbleWine.php',
-    'MediumSans\Menu\SoftDrink'  => __DIR__ . '/classes/SoftDrink.php',
-    'MediumSans\Menu\Beer'       => __DIR__ . '/classes/Beer.php',
-    'MediumSans\Menu\Cocktail'   => __DIR__ . '/classes/Cocktail.php',
-    'MediumSans\Menu\HotDrink'   => __DIR__ . '/classes/HotDrink.php',
+    "MediumSans\BaseClass" => __DIR__ . "/classes/BaseClass.php",
+    "MediumSans\Menu" => __DIR__ . "/classes/Menu.php",
+    "MediumSans\Restaurant" => __DIR__ . "/classes/Restaurant.php",
+    "MediumSans\Menu\MainCourse" => __DIR__ . "/classes/MainCourse.php",
+    "MediumSans\Menu\Starter" => __DIR__ . "/classes/Starter.php",
+    "MediumSans\Menu\Dessert" => __DIR__ . "/classes/Dessert.php",
+    "MediumSans\Menu\RedWine" => __DIR__ . "/classes/RedWine.php",
+    "MediumSans\Menu\WhiteWine" => __DIR__ . "/classes/WhiteWine.php",
+    "MediumSans\Menu\BubbleWine" => __DIR__ . "/classes/BubbleWine.php",
+    "MediumSans\Menu\SoftDrink" => __DIR__ . "/classes/SoftDrink.php",
+    "MediumSans\Menu\Beer" => __DIR__ . "/classes/Beer.php",
+    "MediumSans\Menu\Cocktail" => __DIR__ . "/classes/Cocktail.php",
+    "MediumSans\Menu\HotDrink" => __DIR__ . "/classes/HotDrink.php",
 ]);
 
-Kirby::plugin('mediumsans/foodlab', [
-    'areas' => [
-        'restaurant' => function ($kirby) {
+Kirby::plugin("mediumsans/foodlab", [
+    "areas" => [
+        "menu" => function ($kirby) {
             return [
-                'label'     => 'Restaurant',
-                'menu'      => true,
-                'icon'      => 'food',
-                'link'      => 'foodlab/restaurant',
-                'view'      => 'k-restaurant-view',
-                'views'     => [
-                    require __DIR__ . '/views/restaurant.php',
-                ]
-            ];
-        },
-        'menu' => function ($kirby) {
-            return [
-                'label'     => 'Menu',
-                'menu'      => true,
-                'icon'      => 'file-document',
-                'link'      => 'foodlab/restaurant/menu',
-                'view'      => 'k-menu-view',
-                'views'     => [
-                    require __DIR__ . '/views/menu.php',
-                ],
-                'dialogs'   => [
+                "label" => "Menu",
+                "menu" => true,
+                "icon" => "file-document",
+                "link" => "foodlab/restaurant/menu",
+                "view" => "k-menu-view",
+                "views" => [require __DIR__ . "/views/menu.php"],
+                "dialogs" => [
                     // Beer
-                    require __DIR__ . '/dialogs/menu/beer/fields.php',
-                    require __DIR__ . '/dialogs/menu/beer/create.php',
-                    require __DIR__ . '/dialogs/menu/beer/delete.php',
-                    require __DIR__ . '/dialogs/menu/beer/edit.php',
+                    require __DIR__ . "/dialogs/menu/beer/fields.php",
+                    require __DIR__ . "/dialogs/menu/beer/create.php",
+                    require __DIR__ . "/dialogs/menu/beer/delete.php",
+                    require __DIR__ . "/dialogs/menu/beer/edit.php",
                     // Bubble Wine
-                    require __DIR__ . '/dialogs/menu/bubblewine/fields.php',
-                    require __DIR__ . '/dialogs/menu/bubblewine/create.php',
-                    require __DIR__ . '/dialogs/menu/bubblewine/delete.php',
-                    require __DIR__ . '/dialogs/menu/bubblewine/edit.php',
+                    require __DIR__ . "/dialogs/menu/bubblewine/fields.php",
+                    require __DIR__ . "/dialogs/menu/bubblewine/create.php",
+                    require __DIR__ . "/dialogs/menu/bubblewine/delete.php",
+                    require __DIR__ . "/dialogs/menu/bubblewine/edit.php",
                     // Cocktail
-                    require __DIR__ . '/dialogs/menu/cocktail/fields.php',
-                    require __DIR__ . '/dialogs/menu/cocktail/create.php',
-                    require __DIR__ . '/dialogs/menu/cocktail/delete.php',
-                    require __DIR__ . '/dialogs/menu/cocktail/edit.php',
+                    require __DIR__ . "/dialogs/menu/cocktail/fields.php",
+                    require __DIR__ . "/dialogs/menu/cocktail/create.php",
+                    require __DIR__ . "/dialogs/menu/cocktail/delete.php",
+                    require __DIR__ . "/dialogs/menu/cocktail/edit.php",
                     // Dessert
-                    require __DIR__ . '/dialogs/menu/dessert/fields.php',
-                    require __DIR__ . '/dialogs/menu/dessert/create.php',
-                    require __DIR__ . '/dialogs/menu/dessert/delete.php',
-                    require __DIR__ . '/dialogs/menu/dessert/edit.php',
+                    require __DIR__ . "/dialogs/menu/dessert/fields.php",
+                    require __DIR__ . "/dialogs/menu/dessert/create.php",
+                    require __DIR__ . "/dialogs/menu/dessert/delete.php",
+                    require __DIR__ . "/dialogs/menu/dessert/edit.php",
                     // Hot Drink
-                    require __DIR__ . '/dialogs/menu/hotdrink/fields.php',
-                    require __DIR__ . '/dialogs/menu/hotdrink/create.php',
-                    require __DIR__ . '/dialogs/menu/hotdrink/delete.php',
-                    require __DIR__ . '/dialogs/menu/hotdrink/edit.php',
+                    require __DIR__ . "/dialogs/menu/hotdrink/fields.php",
+                    require __DIR__ . "/dialogs/menu/hotdrink/create.php",
+                    require __DIR__ . "/dialogs/menu/hotdrink/delete.php",
+                    require __DIR__ . "/dialogs/menu/hotdrink/edit.php",
                     // Main Course
-                    require __DIR__ . '/dialogs/menu/maincourse/fields.php',
-                    require __DIR__ . '/dialogs/menu/maincourse/create.php',
-                    require __DIR__ . '/dialogs/menu/maincourse/delete.php',
-                    require __DIR__ . '/dialogs/menu/maincourse/edit.php',
+                    require __DIR__ . "/dialogs/menu/maincourse/fields.php",
+                    require __DIR__ . "/dialogs/menu/maincourse/create.php",
+                    require __DIR__ . "/dialogs/menu/maincourse/delete.php",
+                    require __DIR__ . "/dialogs/menu/maincourse/edit.php",
                     // Red Wine
-                    require __DIR__ . '/dialogs/menu/redwine/fields.php',
-                    require __DIR__ . '/dialogs/menu/redwine/create.php',
-                    require __DIR__ . '/dialogs/menu/redwine/delete.php',
-                    require __DIR__ . '/dialogs/menu/redwine/edit.php',
+                    require __DIR__ . "/dialogs/menu/redwine/fields.php",
+                    require __DIR__ . "/dialogs/menu/redwine/create.php",
+                    require __DIR__ . "/dialogs/menu/redwine/delete.php",
+                    require __DIR__ . "/dialogs/menu/redwine/edit.php",
                     // Soft Drink
-                    require __DIR__ . '/dialogs/menu/softdrink/fields.php',
-                    require __DIR__ . '/dialogs/menu/softdrink/create.php',
-                    require __DIR__ . '/dialogs/menu/softdrink/delete.php',
-                    require __DIR__ . '/dialogs/menu/softdrink/edit.php',
+                    require __DIR__ . "/dialogs/menu/softdrink/fields.php",
+                    require __DIR__ . "/dialogs/menu/softdrink/create.php",
+                    require __DIR__ . "/dialogs/menu/softdrink/delete.php",
+                    require __DIR__ . "/dialogs/menu/softdrink/edit.php",
                     // Starter
-                    require __DIR__ . '/dialogs/menu/starter/fields.php',
-                    require __DIR__ . '/dialogs/menu/starter/create.php',
-                    require __DIR__ . '/dialogs/menu/starter/delete.php',
-                    require __DIR__ . '/dialogs/menu/starter/edit.php',
+                    require __DIR__ . "/dialogs/menu/starter/fields.php",
+                    require __DIR__ . "/dialogs/menu/starter/create.php",
+                    require __DIR__ . "/dialogs/menu/starter/delete.php",
+                    require __DIR__ . "/dialogs/menu/starter/edit.php",
                     // White Wine
-                    require __DIR__ . '/dialogs/menu/whitewine/fields.php',
-                    require __DIR__ . '/dialogs/menu/whitewine/create.php',
-                    require __DIR__ . '/dialogs/menu/whitewine/delete.php',
-                    require __DIR__ . '/dialogs/menu/whitewine/edit.php',
-                ]
+                    require __DIR__ . "/dialogs/menu/whitewine/fields.php",
+                    require __DIR__ . "/dialogs/menu/whitewine/create.php",
+                    require __DIR__ . "/dialogs/menu/whitewine/delete.php",
+                    require __DIR__ . "/dialogs/menu/whitewine/edit.php",
+                ],
             ];
         },
     ],
-    'templates' => [
-      'menu-pdf' => __DIR__ . '/templates/menu-pdf.php',
+    "templates" => [
+        "menu-pdf" => __DIR__ . "/templates/menu-pdf.php",
     ],
-    'api' => require __DIR__ . '/routes/index.php',
+    "blueprints" => [
+        "tabs/restaurant" => __DIR__ . "/blueprints/tabs/restaurant.yml",
+    ],
+    "api" => require __DIR__ . "/routes/index.php",
 ]);
--- cms/site/plugins/kirby-foodlab/routes/index.php
+++ cms/site/plugins/kirby-foodlab/routes/index.php
@@ -54,6 +54,71 @@ return [
                     return Restaurant::create(get());
                 },
             ],
+            [
+                "pattern" => "restaurant",
+                "method" => "GET",
+                "auth" => false,
+                "action" => function () {
+                    $json = [];
+
+                    $json["body"] = [
+                        "btnHero1" => $site->btnHero1()->value(),
+                        "btnHero2" => $site->btnHero2()->value(),
+                        "picHero1" => $site->picHero1()->toFile()->url(),
+                        "picHero2" => $site->picHero2()->toFile()->url(),
+                        "picHero3" => $site->picHero3()->toFile()->url(),
+                        "textHero1" => $site->textHero1()->value(),
+                        "titleFood" => $site->titleFood()->value(),
+                        "textFood" => $site->textFood()->value(),
+                        "fileFood1" => $site->fileFood1()->toFile()->url(),
+                        "btnFood" => $site->btnFood()->value(),
+                        "titleLab" => $site->titleLab()->value(),
+                        "textLab" => $site->textLab()->value(),
+                        "fileLab1" => $site->fileLab1()->toFile()->url(),
+                        "btnLab" => $site->btnLab()->value(),
+                        "picture1" => $site->picture1()->toFile()->url(),
+                        "titleFormation" => $site->titleFormation()->value(),
+                        "textFormation" => $site->textFormation()->value(),
+                        "fileFormation" => $site
+                            ->fileFormation()
+                            ->toFile()
+                            ->url(),
+                        "btnFormation" => $site->btnFormation()->value(),
+                        "titleUnivers" => $site->titleUnivers()->value(),
+                        "subtitleUnivers" => $site->subtitleUnivers()->value(),
+                        "blogUniversTitle1" => $site
+                            ->blogUniversTitle1()
+                            ->value(),
+                        "blogUniversFil1" => $site->blogUniversFil1()->value(),
+                        "blogUniversText1" => $site
+                            ->blogUniversText1()
+                            ->value(),
+                        "blogUniversTitle2" => $site
+                            ->blogUniversTitle2()
+                            ->value(),
+                        "blogUniversFile2" => $site
+                            ->blogUniversFile2()
+                            ->value(),
+                        "blogUniversText2" => $site
+                            ->blogUniversText2()
+                            ->value(),
+                        "titleValues" => $site->titleValues()->value(),
+                        "textValues" => $site->textValues()->value(),
+                        "lstValues" => $site
+                            ->lstValues()
+                            ->toStructure()
+                            ->toArray(),
+                        "footerHeadline" => $site->footerHeadline()->value(),
+                        "textFooter1" => $site->textFooter1()->value(),
+                        "textFooter2" => $site->textFooter2()->value(),
+                        "textFooter3" => $site->textFooter3()->value(),
+                        "btnFooter1" => $site->btnFooter1()->value(),
+                        "btnFooter2" => $site->btnFooter2()->value(),
+                    ];
+
+                    return json_encode($json);
+                },
+            ],
         ];
     },
 ];
