8a5957211dd063b401c7620136c309c21e804419
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-08 10:14:50 +0200

    feat: add load to restaurant page

---
+++ restaurant/src/routes/+page.ts
@@ -0,0 +1,9 @@
+import { variables } from '$lib/utils/constants';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch }) => {
+	const res = await fetch(`${variables.CMS_BASE_URL}/api/restaurant`);
+	const page = await res.json();
+
+	return { page };
+};
