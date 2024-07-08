cf050369e50f2c38e9b033ee87deea9e767b0164
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-08 17:38:55 +0200

    fix: remove Restaurant View

--- cms/site/plugins/kirby-foodlab/src/index.js
+++ cms/site/plugins/kirby-foodlab/src/index.js
@@ -1,9 +1,7 @@
-import RestaurantView from "./components/RestaurantView.vue";
 import MenuView from "./components/MenuView.vue";
 
 panel.plugin("mediumsans/foodlab", {
   components: {
-    'k-restaurant-view': RestaurantView,
-    'k-menu-view': MenuView,
-  }
+    "k-menu-view": MenuView,
+  },
 });
