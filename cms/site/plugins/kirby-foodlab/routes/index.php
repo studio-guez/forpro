3b1385d9ad4017ea0a5eafc53d7d8354e377e812
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-08 13:04:07 +0200

    chore: refactor for consistency

--- cms/site/plugins/kirby-foodlab/routes/index.php
+++ cms/site/plugins/kirby-foodlab/routes/index.php
@@ -60,79 +60,79 @@ return [
                     $json = [];
 
                     $json["hero"] = [
-                        "btnHero1" => $this->site()->btnHero1()->value,
-                        "btnHero2" => $this->site()->btnHero2()->value(),
-                        "picHero1" => $this->site()->picHero1()->toFile() ? $this->site()->picHero1()->toFile()->url() : '',
-                        "picHero2" => $this->site()->picHero2()->toFile() ? $this->site()->picHero2()->toFile()->url() : '',
-                        "picHero3" => $this->site()->picHero3()->toFile() ? $this->site()->picHero3()->toFile()->url() : '',
-                        "textHero1" => $this->site()->textHero1()->kt()->value(),
+                        "btn1" => $this->site()->btnHero1()->value,
+                        "btn2" => $this->site()->btnHero2()->value(),
+                        "pictureURL1" => $this->site()->picHero1()->toFile() ? $this->site()->picHero1()->toFile()->url() : '',
+                        "pictureURL2" => $this->site()->picHero2()->toFile() ? $this->site()->picHero2()->toFile()->url() : '',
+                        "pictureURL3" => $this->site()->picHero3()->toFile() ? $this->site()->picHero3()->toFile()->url() : '',
+                        "text" => $this->site()->textHero1()->kt()->value(),
                     ];
 
                     $json["food"] = [
 
-                        "titleFood" => $this->site()->titleFood()->value(),
-                        "textFood" => $this->site()->textFood()->kt()->value(),
-                        "fileFood1" => $this->site()->fileFood1()->toFile() ? $this->site()->fileFood1()->toFile()->url() : '',
-                        "btnFood" => $this->site()->btnFood()->value()
+                        "title" => $this->site()->titleFood()->value(),
+                        "text" => $this->site()->textFood()->kt()->value(),
+                        "pictureURL" => $this->site()->fileFood1()->toFile() ? $this->site()->fileFood1()->toFile()->url() : '',
+                        "btn" => $this->site()->btnFood()->value()
                     ];
 
                     $json["lab"] = [
-                        "titleLab" => $this->site()->titleLab()->value(),
-                        "textLab" => $this->site()->textLab()->kt()->value(),
-                        "fileLab1" => $this->site()->fileLab1()->toFile() ? $this->site()->fileLab1()->toFile()->url() : '',
-                        "btnLab" => $this->site()->btnLab()->value()
+                        "title" => $this->site()->titleLab()->value(),
+                        "text" => $this->site()->textLab()->kt()->value(),
+                        "pictureURL" => $this->site()->fileLab1()->toFile() ? $this->site()->fileLab1()->toFile()->url() : '',
+                        "btn" => $this->site()->btnLab()->value()
                     ];
 
                     $json["highlight"] = [
-                        "picture1" => $this->site()->picture1()->toFile() ? $this->site()->picture1()->toFile()->url() : '',
+                        "pictureURL" => $this->site()->picture1()->toFile() ? $this->site()->picture1()->toFile()->url() : '',
                     ];
 
                     $json["formation"] = [
-                        "titleFormation" => $this->site()->titleFormation()->value(),
-                        "textFormation" => $this->site()->textFormation()->kt()->value(),
-                        "fileFormation" => $this->site()
+                        "title" => $this->site()->titleFormation()->value(),
+                        "text" => $this->site()->textFormation()->kt()->value(),
+                        "pictureURL" => $this->site()
                             ->fileFormation()
                             ->toFile() ? $this->site()->fileFormation()->toFile()->url() : '',
-                        "btnFormation" => $this->site()->btnFormation()->value()
+                        "btn" => $this->site()->btnFormation()->value()
                     ];
 
                     $json["univers"] = [
-                        "titleUnivers" => $this->site()->titleUnivers()->value(),
-                        "subtitleUnivers" => $this->site()->subtitleUnivers()->value(),
-                        "blogUniversTitle1" => $this->site()
+                        "title" => $this->site()->titleUnivers()->value(),
+                        "subtitle" => $this->site()->subtitleUnivers()->value(),
+                        "blogTitle1" => $this->site()
                             ->blogUniversTitle1()
                             ->value(),
-                        "blogUniversFil1" => $this->site()->blogUniversFil1()->value(),
-                        "blogUniversText1" => $this->site()
+                        "blogPictureUrl1" => $this->site()->blogUniversFil1()->value(),
+                        "blogText1" => $this->site()
                             ->blogUniversText1()
                             ->kt()->value(),
-                        "blogUniversTitle2" => $this->site()
+                        "blogTitle2" => $this->site()
                             ->blogUniversTitle2()
                             ->value(),
-                        "blogUniversFile2" => $this->site()
+                        "blogPictureUrl2" => $this->site()
                             ->blogUniversFile2()
                             ->value(),
-                        "blogUniversText2" => $this->site()
+                        "blogText2" => $this->site()
                             ->blogUniversText2()
                             ->kt()->value()
                     ];
 
                     $json["values"] = [
-                        "titleValues" => $this->site()->titleValues()->value(),
-                        "textValues" => $this->site()->textValues()->value(),
-                        "lstValues" => $this->site()
+                        "title" => $this->site()->titleValues()->value(),
+                        "text" => $this->site()->textValues()->value(),
+                        "list" => $this->site()
                             ->lstValues()
                             ->toStructure()
                             ->toArray()
                     ];
 
                     $json["footer"] = [
-                        "footerHeadline" => $this->site()->footerHeadline()->value(),
-                        "textFooter1" => $this->site()->textFooter1()->kt()->value(),
-                        "textFooter2" => $this->site()->textFooter2()->kt()->value(),
-                        "textFooter3" => $this->site()->textFooter3()->kt()->value(),
-                        "btnFooter1" => $this->site()->btnFooter1()->value(),
-                        "btnFooter2" => $this->site()->btnFooter2()->value()
+                        "Headline" => $this->site()->footerHeadline()->value(),
+                        "text1" => $this->site()->textFooter1()->kt()->value(),
+                        "text2" => $this->site()->textFooter2()->kt()->value(),
+                        "text3" => $this->site()->textFooter3()->kt()->value(),
+                        "btn1" => $this->site()->btnFooter1()->value(),
+                        "btn2" => $this->site()->btnFooter2()->value()
                     ];
 
                     return \Kirby\Http\Response::json(json_encode($json));
