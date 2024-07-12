29ec6ab13a62f1d22374c6fc3b975d50693ed5fb
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-12 17:03:54 +0200

    feat: add meta data and box the template

--- restaurant/src/routes/+layout.svelte
+++ restaurant/src/routes/+layout.svelte
@@ -2,6 +2,22 @@
 	import '../style/_main.scss';
 </script>
 
-<main class="bg-background">
-	<slot />
+<main class="mx-auto grid justify-center">
+	<div class="max-w-[1920px]">
+		<slot />
+	</div>
 </main>
+
+<svelte:head>
+	<title>Foodlab - Imaginé par ForPro</title>
+	<meta
+		name="description"
+		content="Le FoodLab est un des 6 labs dédiés à la formation professionnelle au sein du campus ForPro."
+	/>
+</svelte:head>
+
+<style>
+	:global(body) {
+		background-color: #d2c8b4;
+	}
+</style>
