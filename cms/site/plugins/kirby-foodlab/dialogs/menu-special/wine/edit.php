4ac95fd469b7df4f9e70a94c062fa56217987e88
Samuel Cardoso <samuel@mediumsans.studio>
2024-07-29 13:34:51 +0200

    feat: add menu special dialogs

---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu-special/wine/edit.php
@@ -0,0 +1,21 @@
+<?php
+
+use MediumSans\MenuSpecial\WineSpecial;
+
+return [
+    'pattern' => 'menu/special/wine/(:any)/edit/(:any)',
+    'load'    => function (string $id, string $pageId) {
+        $wine = WineSpecial::find($id, $pageId);
+
+        return [
+            'component' => 'k-form-dialog',
+            'props' => [
+                'fields' => require __DIR__ . '/fields.php',
+                'value' => $wine
+            ]
+        ];
+    },
+    'submit' => function (string $id, string $pageId) {
+        return WineSpecial::update($id, get(), $pageId);
+    }
+];
---
+++ cms/site/plugins/kirby-foodlab/dialogs/menu-special/wine/fields.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'name' => [
+        'label' => 'Nom',
+        'type' => 'text'
+    ],
+    'domain' => [
+        'label' => 'Domaine',
+        'type' => 'text'
+    ],
+    'mill' => [
+        'label' => 'Millésime',
+        'type' => 'text'
+    ],
+    'description' => [
+        'label' => 'Description',
+        'type' => 'text'
+    ],
+];
