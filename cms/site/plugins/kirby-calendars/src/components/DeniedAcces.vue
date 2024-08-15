22d7503fdbc722664b25b6281229824cb8e22764
azertypow <nico@villa1203.ch>
2024-08-15 12:33:15 +0200

    faet: create calendar plugin role user and denied acces view

---
+++ cms/site/plugins/kirby-calendars/src/components/DeniedAcces.vue
@@ -0,0 +1,29 @@
+<template>
+  <k-inside>
+    <k-header>
+      <h1>Access Denied</h1>
+    </k-header>
+
+    <k-text>
+      <p>Sorry, you do not have permission to access this area.</p>
+    </k-text>
+
+  </k-inside>
+</template>
+
+<script>
+export default {
+    name: 'DeniedAccessView',
+    props: {
+        msg: String,
+    },
+    methods: {}
+};
+</script>
+
+<style scoped>
+.k-denied-access {
+    text-align: center;
+    padding: 2rem;
+}
+</style>
