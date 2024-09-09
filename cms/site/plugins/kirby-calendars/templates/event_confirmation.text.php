bdc5ed31b180c95729b5ca21df438c4cf25dc484
azertypow <nico@villa1203.ch>
2024-09-09 11:18:06 +0200

    feat: add mail notification if assignation change

--- cms/site/plugins/kirby-calendars/templates/event_confirmation.text.php
+++ cms/site/plugins/kirby-calendars/templates/event_confirmation.text.php
@@ -2,6 +2,6 @@ Afin de confirmer votre rendez-vous veuillez cliquer sur le lien ci-dessous.
 
 <?= $serviceName ?>
 
-Date et Heure : <?= $startDate ?> à <?= $startTime ?>
+Date et Heure : <?= $startTime ?>
 
 <?= $validationURL ?>
