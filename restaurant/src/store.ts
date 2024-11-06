d8bb19d8e332fa7f71562ccaced2738abc4521bb
azertypow <nico@villa1203.ch>
2024-11-06 11:28:55 +0100

    feat: update mobile button for "cartes et plats du jour"

--- restaurant/src/store.ts
+++ restaurant/src/store.ts
@@ -1,6 +1,8 @@
 import {type Writable, writable} from "svelte/store";
 import type {ISiteInfo} from "$lib/interfaces/cmsApiResponse";
 
+export const linkTreeIsOpen = writable(false)
+
 export const menuIsOpen = writable(false)
 
 export const siteInfo: Writable<ISiteInfo | null> = writable(null)
