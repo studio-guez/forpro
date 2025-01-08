1bcaaea4343d8cdca2362605e8860d1639fa3111
azertypow <nico@villa1203.ch>
2025-01-08 15:07:19 +0100

    feat: add matomo for restaurant website

--- restaurant/src/routes/+layout.svelte
+++ restaurant/src/routes/+layout.svelte
@@ -1,5 +1,40 @@
+<svelte:head>
+
+	<!-- Matomo -->
+	<script>
+		var _paq = window._paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//matomo.for-pro.ch/";
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setSiteId', '2']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<!-- End Matomo Code -->
+
+</svelte:head>
+
+
 <script lang="ts">
 	import '../style/_main.scss';
+	import {afterNavigate} from "$app/navigation";
+
+	declare var _paq: unknown
+
+	afterNavigate((navigation) => {
+		if (_paq) {
+			_paq.push(['setCustomUrl', '/' + window.location.href])
+			_paq.push(['setDocumentTitle', window.location.pathname])
+			_paq.push(['setReferrerUrl', navigation.from])
+			_paq.push(['trackPageView'])
+		}
+	})
+
+
 </script>
 
 <main class="mx-auto grid justify-center overflow-hidden lg:overflow-auto">
