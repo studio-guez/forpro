b5447410785b4e602e7bf214385ac8165e296f49
azertypow <nico@villa1203.ch>
2025-01-08 15:10:54 +0100

    chore: move <svelte:head> on top of the file (readability)

--- restaurant/src/routes/+layout.svelte
+++ restaurant/src/routes/+layout.svelte
@@ -1,3 +1,27 @@
+<svelte:head>
+	<title>Foodlab - Imaginé par ForPro</title>
+	<meta
+					name="description"
+					content="Le FoodLab est un des 6 labs dédiés à la formation professionnelle au sein du campus ForPro."
+	/>
+
+	<!-- Matomo -->
+	<script>
+		var _paq = window._paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//matomo.for-pro.ch/";
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setSiteId', '2']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<!-- End Matomo Code -->
+</svelte:head>
+
 <script lang="ts">
 	import '../style/_main.scss';
 	import {afterNavigate} from "$app/navigation";
@@ -21,30 +45,6 @@
 		<slot />
 	</div>
 </main>
-
-<svelte:head>
-	<title>Foodlab - Imaginé par ForPro</title>
-	<meta
-		name="description"
-		content="Le FoodLab est un des 6 labs dédiés à la formation professionnelle au sein du campus ForPro."
-	/>
-
-	<!-- Matomo -->
-	<script>
-		var _paq = window._paq = window._paq || [];
-		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		(function() {
-			var u="//matomo.for-pro.ch/";
-			_paq.push(['setTrackerUrl', u+'matomo.php']);
-			_paq.push(['setSiteId', '2']);
-			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-		})();
-	</script>
-	<!-- End Matomo Code -->
-</svelte:head>
 
 <style>
 	:global(body) {
