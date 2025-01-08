df2f0b170a7500e1e2b049f4e46ccdb101680d56
azertypow <nico@villa1203.ch>
2025-01-08 15:09:27 +0100

    fix: A component can only have one <svelte:head> tag

--- restaurant/src/routes/+layout.svelte
+++ restaurant/src/routes/+layout.svelte
@@ -1,24 +1,3 @@
-<svelte:head>
-
-	<!-- Matomo -->
-	<script>
-		var _paq = window._paq = window._paq || [];
-		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		(function() {
-			var u="//matomo.for-pro.ch/";
-			_paq.push(['setTrackerUrl', u+'matomo.php']);
-			_paq.push(['setSiteId', '2']);
-			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-		})();
-	</script>
-	<!-- End Matomo Code -->
-
-</svelte:head>
-
-
 <script lang="ts">
 	import '../style/_main.scss';
 	import {afterNavigate} from "$app/navigation";
@@ -49,6 +28,22 @@
 		name="description"
 		content="Le FoodLab est un des 6 labs dédiés à la formation professionnelle au sein du campus ForPro."
 	/>
+
+	<!-- Matomo -->
+	<script>
+		var _paq = window._paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//matomo.for-pro.ch/";
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setSiteId', '2']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<!-- End Matomo Code -->
 </svelte:head>
 
 <style>
