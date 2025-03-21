db9a3de1eaab5f82a5e18b85bec5a5df8964fe37
azertypow <nico@villa1203.ch>
2025-03-21 08:27:04 +0100

    feat: dropdown max width

--- website/src/lib/components/BlockDropdown.svelte
+++ website/src/lib/components/BlockDropdown.svelte
@@ -62,6 +62,13 @@
 </div>
 
 <style lang="scss">
+  .s-dropdown {
+    max-width: 70rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+
   .s-dropdown__item {
     --s-dropdown-line-width: 3px;
     --s-dropdown-line-spacing: 10px;
