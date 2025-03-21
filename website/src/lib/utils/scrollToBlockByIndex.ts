5fd772ec684d431620a1eb637bfb30a4974f8be2
azertypow <nico@villa1203.ch>
2025-03-21 17:55:00 +0100

    chore: clean

--- website/src/lib/utils/scrollToBlockByIndex.ts
+++ website/src/lib/utils/scrollToBlockByIndex.ts
@@ -2,8 +2,6 @@ export function scrollToBlockByIndex(blocIndexInfo: "" | number | undefined) {
 
     if(typeof blocIndexInfo !== 'number') return
 
-    console.log( blocIndexInfo )
-
     const elementToScroll = document?.querySelector(`.s-page__content > *:nth-child(${blocIndexInfo})`)
 
     if( !elementToScroll ) return
