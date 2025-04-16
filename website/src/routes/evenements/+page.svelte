27cfa423dc7a23a6eb9150a610e75b7526ddae50
azertypow <nico@villa1203.ch>
2025-04-16 13:27:40 +0200

    feat: use custom text button

--- website/src/routes/evenements/+page.svelte
+++ website/src/routes/evenements/+page.svelte
@@ -82,7 +82,13 @@
                     <div class="s-evenements__events__events-wrap__item__details">
                         <a class="app-button app-button--rounded"
                            href="{event.pageContent.uri}"
-                        >En savoir plus</a>
+                        >
+                            {#if event.pageContent.content.parent_page_link_text && event.pageContent.content.parent_page_link_text.length > 0}
+                                {event.pageContent.content.parent_page_link_text}
+                            {:else }
+                            En savoir plus
+                            {/if}
+                        </a>
                     </div>
                 </div>
             {/each}
