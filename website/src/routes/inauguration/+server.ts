aa33ead45e7b60f6708a75c0fc3caaeeb577f798
azertypow <nico@villa1203.ch>
2025-06-20 14:03:01 +0200

    feat: new link

--- website/src/routes/inauguration/+server.ts
+++ website/src/routes/inauguration/+server.ts
@@ -4,7 +4,7 @@ export const GET: RequestHandler = () => {
     return new Response(null, {
         status: 302,
         headers: {
-            Location: 'https://invitation.infomaniak.com/forms/i/demo?inv=7bcb87c7428790c102dc96bf122ec06c569f3d584eecea56744f610a2b00dbbb#/'
+            Location: 'https://framaforms.org/inauguration-de-forpro-1750417762'
         }
     })
 }
