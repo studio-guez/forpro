fcfd775a36fbe3f35c56f7193dad8c6e81058b0f
azertypow <nico@villa1203.ch>
2026-02-17 00:10:10 +0100

    feat: align foodLab week view with FoodCourt week view

--- cms/site/plugins/kirby-menu-du-jour/dialogs/fodd-lab/fields.php
+++ cms/site/plugins/kirby-menu-du-jour/dialogs/fodd-lab/fields.php
@@ -1,16 +1,41 @@
 <?php
 
-return [
+$daysOfWeek = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+$fields = [
     'date' => [
-        'label' => 'Date',
-        'type' => 'date',
-    ],
-    'menu' => [
-        'label' => 'Menu',
-        'type' => 'text',
+      'label' => 'Date (le jours sélectionné représente la semaine entière)',
+      'type' => 'date',
+      'time'    => false,
+      'width' => '1/2',
     ],
     'prix' => [
-        'label' => 'Prix',
-        'type' => 'text',
+        'label' => 'Prix unique sur toute la semaine',
+        'type' => 'number',
+        'min' => 0,
+        'placeholder' => '0.00',
+        'step' => '0.5',
+        'width' => '1/2'
     ],
 ];
+
+
+for ($jour = 1; $jour <= 5; $jour++) {
+  $fields["jour{$jour}_info"] = [
+    'label' => $daysOfWeek[$jour - 1],
+    'type' => 'headline',
+    'width' => '2/2'
+  ];
+
+  $fields["jour{$jour}_menu"] = [
+    'label' => 'Menu du jour',
+    'type' => 'writer',
+    'marks' => ['italic'],
+    'nodes' => false,
+    'inline' => true,
+    'width' => '1/2'
+  ];
+}
+
+
+return $fields;
