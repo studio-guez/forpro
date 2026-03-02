4c4c2d140a4b73dc32121e3ead9a51c16cb46338
azertypow <nico@villa1203.ch>
2026-03-02 07:37:38 +0100

    fix: vegetarian_suggestion not saved
    replace by jour6_menu

--- cms/site/plugins/kirby-menu-du-jour/classes/FoddLab.php
+++ cms/site/plugins/kirby-menu-du-jour/classes/FoddLab.php
@@ -59,7 +59,7 @@ class FoddLab extends BaseClass
             "jour3_menu" => $input["jour3_menu"] ?? "",
             "jour4_menu" => $input["jour4_menu"] ?? "",
             "jour5_menu" => $input["jour5_menu"] ?? "",
-            "vegetarian_suggestion" => $input["vegetarian_suggestion"] ?? "",
+            "jour6_menu" => $input["jour6_menu"] ?? "",
         ];
 
         $data = static::readAll();
@@ -79,7 +79,7 @@ class FoddLab extends BaseClass
             "jour3_menu" => $input["jour3_menu"] ?? "",
             "jour4_menu" => $input["jour4_menu"] ?? "",
             "jour5_menu" => $input["jour5_menu"] ?? "",
-            "vegetarian_suggestion" => $input["vegetarian_suggestion"] ?? "",
+            "jour6_menu" => $input["jour6_menu"] ?? "",
         ];
 
         $data = static::readAll();
