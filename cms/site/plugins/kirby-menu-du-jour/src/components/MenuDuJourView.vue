5ff0b6506248aac812183005c24934db7c4b7f0c
azertypow <nico@villa1203.ch>
2026-03-09 16:09:38 +0100

    fix: typo

--- cms/site/plugins/kirby-menu-du-jour/src/components/MenuDuJourView.vue
+++ cms/site/plugins/kirby-menu-du-jour/src/components/MenuDuJourView.vue
@@ -201,7 +201,7 @@
       tag="h3"
     >
       Information dans le bas du menu imprimé du FoodLab
-      — <span v-if="isSaving" style="opacity: 0.5; font-weight: normal"> – sauvegarde…</span>
+      <span v-if="isSaving" style="opacity: 0.5; font-weight: normal"> – sauvegarde…</span>
       <span v-else-if="hasSaved" style="opacity: 0.5; font-weight: normal"> – sauvegardé</span>
     </k-headline>
 
