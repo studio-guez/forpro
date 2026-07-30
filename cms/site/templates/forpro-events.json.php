a40dda93668972c1606a7bc1257473c70c547850
OctoY <yann@octoplus.solutions>
2026-07-30 17:01:09 +0200

    fix: implement SEO functionality properly so it is used in the frontend

--- cms/site/templates/forpro-events.json.php
+++ cms/site/templates/forpro-events.json.php
@@ -28,14 +28,6 @@ $body = $page->body()->toBlocks()->map(function ($item){
     ];
 })->data();
 
-function getValueNotEmpty($pageAttribute, $siteAttribute) {
-    if($pageAttribute->isNotEmpty()) {
-        return $pageAttribute->value();
-    } elseif($siteAttribute->isNotEmpty()) {
-        return $siteAttribute->value();
-    }
-    return "";
-}
 
 $json['options'] = [
     'showMenu' => $showMenu,
@@ -46,26 +38,7 @@ $json['options'] = [
 
 $json['body'] = $body;
 
-$json['seo'] = [
-    // General ===========================================================
-    'metaTemplate'      => getValueNotEmpty($page->metaTemplate(), $site->metaTemplate()),
-    'metaDescription'   => getValueNotEmpty($page->metaDescription(), $site->metaDescription()),
-    'metaAuthor'        => getValueNotEmpty($page->metaAuthor(), $site->metaAuthor()),
-    'metaImage'         => getValueNotEmpty($page->metaImage(), $site->metaImage()),
-    'metaPhoneNumber'   => getValueNotEmpty($page->metaPhoneNumber(), $site->metaPhoneNumber()),
-    // Facebook ===========================================================
-    'ogTemplate'        => getValueNotEmpty($page->ogTemplate(), $site->ogTemplate()),
-    'ogDescription'     => getValueNotEmpty($page->ogDescription(), $site->ogDescription()),
-    'ogImage'           => getValueNotEmpty($page->ogImage(), $site->ogImage()),
-    'ogSiteName'        => getValueNotEmpty($page->ogSiteName(), $site->ogSiteName()),
-    // Twitter ===========================================================
-    'twitterTemplate'   => getValueNotEmpty($page->twitterTemplate(), $site->twitterTemplate()),
-    'twitterDescription'=> getValueNotEmpty($page->twitterDescription(), $site->twitterDescription()),
-    'twitterImage'      => getValueNotEmpty($page->twitterImage(), $site->twitterImage()),
-    'twitterCardType'   => getValueNotEmpty($page->twitterCardType(), $site->twitterCardType()),
-    'twitterSite'       => getValueNotEmpty($page->twitterSite(), $site->twitterSite()),
-    'twitterCreator'    => getValueNotEmpty($page->twitterCreator(), $site->twitterCreator()),
-];
+$json['seo'] = $page->seoData();
 
 
 
