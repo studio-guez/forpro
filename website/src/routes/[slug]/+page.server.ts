bb5d448c701b5075bc53351ef52778ca61c172c8
OctoY <yann@octoplus.solutions>
2026-08-03 13:31:52 +0200

    fix: enhance error handling in page server loads to return appropriate status messages

--- website/src/routes/[slug]/+page.server.ts
+++ website/src/routes/[slug]/+page.server.ts
@@ -1,3 +1,4 @@
+import {error} from "@sveltejs/kit";
 import {fetchFromAPI} from "$lib/utils/shared";
 import {type IPage} from "$lib/interfaces/cmsApiResponse";
 import {variables} from "$lib/utils/constants";
@@ -11,5 +12,9 @@ export const load: PageServerLoad = async ({params}) => {
         method: 'GET',
     })
 
-    return await fetchFromAPI<IPage>(request, 'Failed to fetch page data')
+    const data = await fetchFromAPI<IPage>(request, 'Failed to fetch page data')
+
+    if (!data) error(404, {message: 'Cette page n\'existe pas.'})
+
+    return data
 }
