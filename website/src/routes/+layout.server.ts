8881b750c3df294328096da2e9a09a3a65b8e44a
OctoY <yann@octoplus.solutions>
2026-08-03 14:04:43 +0200

    fix: enhance error handling for API responses across multiple routes

--- website/src/routes/+layout.server.ts
+++ website/src/routes/+layout.server.ts
@@ -2,11 +2,11 @@ import type {ISiteInfo} from "$lib/interfaces/cmsApiResponse";
 import {variables} from "$lib/utils/constants";
 import {error} from "@sveltejs/kit";
 import {fetchFromAPI} from "$lib/utils/shared";
-import type {PageServerLoad} from "../../.svelte-kit/types/src/routes/$types";
+import type {LayoutServerLoad} from "./$types";
 
 export const prerender = false;
 
-export const load: PageServerLoad = async () => {
+export const load: LayoutServerLoad = async () => {
 
     const request = new Request(`${variables.CMS_BASE_URL}/site-info.json`, {
         method: 'GET',
