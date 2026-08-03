f50150f96714a7fe877ee320bc73c2eb5d59e922
OctoY <yann@octoplus.solutions>
2026-08-03 13:31:52 +0200

    fix: enhance error handling in page server loads to return appropriate status messages

--- website/src/routes/+layout.server.ts
+++ website/src/routes/+layout.server.ts
@@ -1,5 +1,6 @@
 import type {ISiteInfo} from "$lib/interfaces/cmsApiResponse";
 import {variables} from "$lib/utils/constants";
+import {error} from "@sveltejs/kit";
 import {fetchFromAPI} from "$lib/utils/shared";
 import type {PageServerLoad} from "../../.svelte-kit/types/src/routes/$types";
 
@@ -11,5 +12,9 @@ export const load: PageServerLoad = async () => {
         method: 'GET',
     })
 
-    return await fetchFromAPI<ISiteInfo>(request, 'Failed to fetch page data')
+    const data = await fetchFromAPI<ISiteInfo>(request, 'Failed to fetch page data')
+
+    if (!data) error(502, {message: 'Impossible de charger les informations du site.'})
+
+    return data
 }
