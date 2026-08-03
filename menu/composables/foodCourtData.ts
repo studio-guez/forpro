39aa2ed67217869ca66f31412d535b2e4facd17b
OctoY <yann@octoplus.solutions>
2026-08-03 12:28:13 +0200

    fix: remove console.info statements from foodCourtData and foodLabData functions

--- menu/composables/foodCourtData.ts
+++ menu/composables/foodCourtData.ts
@@ -120,7 +120,5 @@ export function foodCourt_GetCurrentWeekMenu(menus: IMenuData__foodCourt): IMenu
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
     [0] || null
 
-  console.info(currentWeekMenu)
-
   return currentWeekMenu
 }
--- menu/composables/foodLabData.ts
+++ menu/composables/foodLabData.ts
@@ -36,7 +36,5 @@ export function foodLab_GetCurrentWeekMenu(menus: IMenuData__foodLab): IMenuData
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
     [0] || null
 
-  console.info(currentWeekMenu)
-
   return currentWeekMenu
 }
