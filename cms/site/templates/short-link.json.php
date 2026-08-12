21dc3f8e943d269d3eacdd6f1eaa43c1931dab73
OctoY <yann@octoplus.solutions>
2026-08-12 14:06:03 +0200

    Remove unused JSON and PHP template files to streamline the codebase

--- cms/site/templates/short-link.json.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo json_encode([
-  'title' => $page->title()->value(),
-  'url'   => $page->url_redirection()->value(),
-]);
--- cms/site/templates/site-info.php
+++
@@ -1,3 +0,0 @@
-<!-- Content is on site-info.json.php -->
-<?php
-
