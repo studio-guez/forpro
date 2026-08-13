10403e0fc05c322222e749c00b045775b8575941
OctoY <yann@octoplus.solutions>
2026-08-13 16:10:21 +0200

    Add additional page properties to JSON output for overtitles, title arrow, and arrow color

--- cms/site/templates/page.json.php
+++ cms/site/templates/page.json.php
@@ -11,11 +11,17 @@ $json = [];
 $json['title'] = $page->title()->value();
 $json['slug'] = $page->slug();
 
+$json['overtitle'] = $page->overtitle()->value();
+$json['titleHasArrow'] = $page->titleHasArrow()->toBool();
+$json['arrowColor'] = $page->arrowColor()->value();
+
 $coverFile = $page->cover()->toFile();
 $json['cover'] = $coverFile ? Utils::getJsonEncodeImageData($coverFile) : null;
 
 $json['body'] = $page->body()->toBlocks()->toArray();
 
 $json['seo'] = Utils::getSeoDataFromPage($page);
+
+$json['trackWithMatomo'] = $page->trackWithMatomo()->toBool();
 
 echo json_encode($json);
