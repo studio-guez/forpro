1d3c8cc6e5e53e1d41c45bb271de528623def6a7
OctoY <yann@octoplus.solutions>
2026-08-15 16:02:05 +0200

    Add SVG icons for Arrow, Email, and Plus components

---
+++ website/src/lib/components/svg/IconPlus.svelte
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="37" height="38" viewBox="0 0 37 38" fill="none" aria-hidden="true">
+	<path d="M7.70825 19.0001H29.2916M18.4999 7.91675V30.0834" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
