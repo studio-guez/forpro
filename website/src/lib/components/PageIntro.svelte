e5d12051592ff7cff17b76a32103f798777f8380
OctoY <yann@octoplus.solutions>
2026-08-15 17:34:50 +0200

    Add "Retour à" prefix to parent page link for improved clarity

--- website/src/lib/components/PageIntro.svelte
+++ website/src/lib/components/PageIntro.svelte
@@ -38,7 +38,7 @@
 <section class="py-18 px-base relative" aria-labelledby="page-intro-title">
 	{#if parentPage}
 		<div class="mb-6 mt-[calc(-1.5rem-1lh)]">
-			<a href="/{parentPage.path}" style:color="var(--color-{themeColor})" class="text-label group"><span class="inline-block group-hover:-translate-x-0.75 transition-transform">&larr;</span> {parentPage.title}</a>
+			<a href="/{parentPage.path}" style:color="var(--color-{themeColor})" class="text-label group"><span class="inline-block group-hover:-translate-x-0.75 transition-transform">&larr;</span> Retour à {parentPage.title}</a>
 		</div>
 	{/if}
 	{#if isTwoCol}
