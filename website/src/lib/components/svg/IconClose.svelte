c1b703e6a6d8a1b4f867af4ff5cc7d434c024f77
OctoY <yann@octoplus.solutions>
2026-08-16 14:15:58 +0200

    feat: update stroke color in SVG icons to use currentColor for better theming

--- website/src/lib/components/svg/IconClose.svelte
+++ website/src/lib/components/svg/IconClose.svelte
@@ -9,5 +9,5 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 31 32" fill="none" aria-hidden="true" class={className}>
-  <path d="M22.2865 10.021L9.55139 22.821M9.55139 10.021L22.2865 22.821" stroke="#1754FF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M22.2865 10.021L9.55139 22.821M9.55139 10.021L22.2865 22.821" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
