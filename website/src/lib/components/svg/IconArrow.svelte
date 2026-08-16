6d9795dbca1fb588c2114e3a82b612a0160a80b8
OctoY <yann@octoplus.solutions>
2026-08-16 13:18:05 +0200

    Add class prop support to SVG icon components for styling flexibility

--- website/src/lib/components/svg/IconArrow.svelte
+++ website/src/lib/components/svg/IconArrow.svelte
@@ -1,3 +1,11 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="37" height="38" viewBox="0 0 37 38" fill="none" aria-hidden="true">
+<script lang="ts">
+	interface Props {
+		class?: string;
+	}
+
+	let { class: className = '' }: Props = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="37" height="38" viewBox="0 0 37 38" fill="none" aria-hidden="true" class={className}>
 	<path d="M7.70801 19.0003H29.2913M18.4997 30.0837L29.2913 19.0003L18.4997 7.91699" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
--- website/src/lib/components/svg/IconHamburger.svelte
+++ website/src/lib/components/svg/IconHamburger.svelte
@@ -1,3 +1,11 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+<script lang="ts">
+	interface Props {
+		class?: string;
+	}
+
+	let { class: className = '' }: Props = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none" class={className}>
   <path d="M6 7.53125H22.5M6 14.75H22.5M6 21.9687H22.5" stroke="#1754FF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
