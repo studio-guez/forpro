f93d624ce2b6a3653064fad62f992b98cd3167ed
OctoY <yann@octoplus.solutions>
2026-08-16 14:05:32 +0200

    feat: add width and height props to SVG icons for better customization
    - Updated IconInstagram, IconLink, IconLinkedin, IconPlus, IconSnapchat, IconTiktok, IconX, and IconYoutube components to accept width and height props.
    - Default dimensions set for each icon to maintain existing appearance.

--- website/src/lib/components/svg/IconX.svelte
+++ website/src/lib/components/svg/IconX.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	interface Props {
 		class?: string;
+		width?: number | string;
+		height?: number | string;
 	}
 
-	let { class: className = '' }: Props = $props();
+	let { class: className = '', width = 24, height = 24 }: Props = $props();
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="24" height="24" fill="currentColor" aria-hidden="true" class={className}><!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M453.2 112L523.8 112L369.6 288.2L551 528L409 528L297.7 382.6L170.5 528L99.8 528L264.7 339.5L90.8 112L236.4 112L336.9 244.9L453.2 112zM428.4 485.8L467.5 485.8L215.1 152L173.1 152L428.4 485.8z"/></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" {width} {height} fill="currentColor" aria-hidden="true" class={className}><!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M453.2 112L523.8 112L369.6 288.2L551 528L409 528L297.7 382.6L170.5 528L99.8 528L264.7 339.5L90.8 112L236.4 112L336.9 244.9L453.2 112zM428.4 485.8L467.5 485.8L215.1 152L173.1 152L428.4 485.8z"/></svg>
