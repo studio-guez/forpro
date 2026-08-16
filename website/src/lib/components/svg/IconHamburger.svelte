86d38610c2b3e2b90a9d79ab5455c3291fd913e6
OctoY <yann@octoplus.solutions>
2026-08-16 13:13:43 +0200

    Add hamburger icon SVG component

---
+++ website/src/lib/components/svg/IconHamburger.svelte
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+  <path d="M6 7.53125H22.5M6 14.75H22.5M6 21.9687H22.5" stroke="#1754FF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
