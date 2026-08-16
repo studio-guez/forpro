bde7b226a6001348e671671b722b0bfbd265e01c
OctoY <yann@octoplus.solutions>
2026-08-16 14:00:54 +0200

    Add IconClose component with SVG markup and TypeScript props

---
+++ website/src/lib/components/svg/IconClose.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	interface Props {
+		class?: string;
+	}
+
+	let { class: className = '' }: Props = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="31" height="32" viewBox="0 0 31 32" fill="none" aria-hidden="true" class={className}>
+  <path d="M22.2865 10.021L9.55139 22.821M9.55139 10.021L22.2865 22.821" stroke="#1754FF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
