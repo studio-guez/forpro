9ff79da5aa51d6b2f230bd7a6fee8ba915bde68d
OctoY <yann@octoplus.solutions>
2026-08-17 13:06:43 +0200

    feat: enhance body structure handling for module-titre-texte-image blocks

--- cms/site/templates/page.json.php
+++ cms/site/templates/page.json.php
@@ -24,7 +24,30 @@ $json['introCta'] = Utils::resolveCtaStructure($page->introCta());
 $coverFile = $page->cover()->toFile();
 $json['cover'] = $coverFile ? Utils::getJsonEncodeImageData($coverFile) : null;
 
-$json['body'] = $page->body()->toBlocks()->toArray();
+$blocks = [];
+foreach ($page->body()->toBlocks() as $block) {
+    $content = [];
+    if ($block->type() === 'module-titre-texte-image') {
+        $imageFile  = $block->image()->toFile();
+        $content = [
+            'title'         => $block->title()->value(),
+            'description'   => $block->description()->value(),
+            'image'         => $imageFile ? Utils::getJsonEncodeImageData($imageFile) : null,
+            'imagePosition' => $block->content()->get('image_position')->or('right')->value(),
+            'variant'       => $block->variant()->or('default')->value(),
+            'cta'           => Utils::resolveCtaStructure($block->cta()),
+        ];
+    } else {
+        $content = $block->toArray()['content'] ?? [];
+    }
+    $blocks[] = [
+        'id'       => $block->id(),
+        'type'     => $block->type(),
+        'isHidden' => $block->isHidden(),
+        'content'  => $content,
+    ];
+}
+$json['body'] = $blocks;
 
 $json['seo'] = Utils::getSeoDataFromPage($page);
 
