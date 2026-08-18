527eaca622308747a29823896a1b36b2ac371759
OctoY <yann@octoplus.solutions>
2026-08-18 16:16:42 +0200

    fix: remove unnecessary margin class from SearchInput for cleaner styling

--- website/src/lib/components/SiteHeader.svelte
+++ website/src/lib/components/SiteHeader.svelte
@@ -117,7 +117,7 @@
 					aria-label="Rechercher"
 					tabindex={searchOpen ? undefined : -1}
 					onblur={() => (searchOpen = false)}
-					class="text-body-2 font-bold text-blue bg-transparent border-0 min-w-0 w-0 opacity-0 group-has-[#search-button:hover]:w-48 group-has-[#search-button:hover]:opacity-100 group-has-[#search-button:hover]:mr-3 focus:w-48 focus:opacity-100 focus:mr-3 transition-all duration-300 ease-out placeholder:text-blue/50 focus:ring-0 focus:outline-none peer"
+					class="text-body-2 font-bold text-blue bg-transparent border-0 min-w-0 w-0 opacity-0 group-has-[#search-button:hover]:w-48 group-has-[#search-button:hover]:opacity-100 focus:w-48 focus:opacity-100 transition-all duration-300 ease-out placeholder:text-blue/50 focus:ring-0 focus:outline-none peer"
 				/>
 				<button
 					type="button"
