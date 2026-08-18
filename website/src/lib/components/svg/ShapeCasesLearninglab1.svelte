64580c73648ce3868d680142ab2ee08ee265a377
OctoY <yann@octoplus.solutions>
2026-08-18 16:50:58 +0200

    feat: add multiple SVG components with customizable dimensions and class support

---
+++ website/src/lib/components/svg/ShapeCasesLearninglab1.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		class?: string;
+		width?: number | string;
+		height?: number | string;
+	}
+
+	let { class: className = '', width = 459, height = 567 }: Props = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 459 567" fill="none" aria-hidden="true" class={className}>
+	<path d="M126.147 515.734L48.9662 413.919L314.239 212.829L391.42 314.644L458.044 264.139L8.39258 -329.031L-58.2319 -278.527L15.9158 -180.713L-249.357 20.3772L-323.505 -77.4367L-390.048 -26.994L59.6038 566.176L126.147 515.734Z" fill="currentColor"/>
+</svg>
---
+++ website/src/lib/components/svg/ShapeCasesMakerlab1.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		class?: string;
+		width?: number | string;
+		height?: number | string;
+	}
+
+	let { class: className = '', width = 376, height = 521 }: Props = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 376 521" fill="none" aria-hidden="true" class={className}>
+	<path d="M267.083 375.452V87.719C332.594 73.5857 375.794 49.4079 375.794 21.8523C375.794 -21.7032 267.438 -57.0811 133.749 -57.0811C0.060454 -57.0811 -108.295 -21.7032 -108.295 21.8523C-108.295 49.4079 -65.0951 73.5857 0.416009 87.719V375.452C-65.0951 389.586 -108.295 413.852 -108.295 441.319C-108.295 484.875 0.060454 520.252 133.749 520.252C267.438 520.252 375.794 484.875 375.794 441.319C375.794 413.763 332.594 389.586 267.083 375.452Z" fill="currentColor"/>
+</svg>
