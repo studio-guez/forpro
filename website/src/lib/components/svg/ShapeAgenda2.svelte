d1abf7d6ff1cd40f49bf42e3868e01cfcaef98b0
OctoY <yann@octoplus.solutions>
2026-08-21 13:10:23 +0200

    Add SVG components for ShapeAgenda1, ShapeAgenda2, ShapeProjets1, and ShapeProjets2 with customizable properties

---
+++ website/src/lib/components/svg/ShapeAgenda2.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		class?: string;
+		width?: number | string;
+		height?: number | string;
+	}
+
+	let { class: className = '', width = 289, height = 476 }: Props = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 289 476" fill="none" aria-hidden="true" class={className}>
+	<path d="M138.506 475.264H160.642C237.187 475.264 299.147 413.304 299.147 336.758V28.5081L149.574 -235.573L0 28.5081V336.758C0 413.2 61.9603 475.264 138.506 475.264Z" fill="currentColor"/>
+</svg>
