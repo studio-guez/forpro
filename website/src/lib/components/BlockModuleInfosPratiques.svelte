505a1e41103fe55b9d8e55c8403f2b3f3b8d3262
OctoY <yann@octoplus.solutions>
2026-08-21 10:29:36 +0200

    Update BlockModuleInfosPratiques to replace 'subtitle' with 'shortDesc' for improved content display

--- website/src/lib/components/BlockModuleInfosPratiques.svelte
+++ website/src/lib/components/BlockModuleInfosPratiques.svelte
@@ -47,7 +47,7 @@
 		>
 			{content.title}
 		</h2>
-		<p class="text-body-1 font-bold mt-9" style:color={textColor}>{content.subtitle}</p>
+		<div class="prose text-body-1 font-bold mt-9" style:color={textColor}>{@html content.shortDesc}</div>
 	</div>
 
 	{#if content.elements.length > 0}
