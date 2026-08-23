8a898e7e3cc79758d251759e1000be2911eb2443
OctoY <yann@octoplus.solutions>
2026-08-23 19:37:41 +0200

    Add Blocks component integration to Events, Faq, Page, and Projects components

--- website/src/lib/components/Page.svelte
+++ website/src/lib/components/Page.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
 	import PageHero from '$lib/components/PageHero.svelte';
 	import PageIntro from '$lib/components/PageIntro.svelte';
-	import BlockModuleTitreTexteImage from '$lib/components/BlockModuleTitreTexteImage.svelte';
-	import BlockModuleCases from '$lib/components/BlockModuleCases.svelte';
-	import BlockModuleGrilleImages from '$lib/components/BlockModuleGrilleImages.svelte';
-	import BlockModuleInfosPratiques from '$lib/components/BlockModuleInfosPratiques.svelte';
-	import BlockModuleAgenda from '$lib/components/BlockModuleAgenda.svelte';
-	import BlockModuleProjets from '$lib/components/BlockModuleProjets.svelte';
-	import BlockModuleCta from '$lib/components/BlockModuleCta.svelte';
-	import BlockModulePartenaires from '$lib/components/BlockModulePartenaires.svelte';
-	import type { Page, ModuleTitreTexteImageContent, ModuleCasesContent, ModuleGrilleImagesContent,ModuleInfosPratiquesContent, ModuleAgendaContent, ModuleProjetsContent, ModuleCtaContent, ModulePartenairesContent } from '$lib/interfaces/page';
+	import Blocks from '$lib/components/Blocks.svelte';
+	import type { Page } from '$lib/interfaces/page';
 
 	let { page }: { page: Page } = $props();
 </script>
@@ -26,43 +19,4 @@
 	titleImage={page.introTitleImage}
 />
 
-{#each page.body as block (block.id)}
-	{#if !block.isHidden}
-		{#if block.type === 'module-titre-texte-image'}
-			<BlockModuleTitreTexteImage
-				content={block.content as unknown as ModuleTitreTexteImageContent}
-				theme={page.theme}
-			/>
-		{:else if block.type === 'module-cases'}
-			<BlockModuleCases
-				content={block.content as unknown as ModuleCasesContent}
-				theme={page.theme}
-			/>
-		{:else if block.type === 'module-grille-images'}
-			<BlockModuleGrilleImages
-				content={block.content as unknown as ModuleGrilleImagesContent}
-				theme={page.theme}
-			/>
-		{:else if block.type === 'module-infos-pratiques'}
-			<BlockModuleInfosPratiques
-				content={block.content as unknown as ModuleInfosPratiquesContent}
-			/>
-		{:else if block.type === 'module-agenda'}
-			<BlockModuleAgenda
-				content={block.content as unknown as ModuleAgendaContent}
-			/>
-		{:else if block.type === 'module-projets'}
-			<BlockModuleProjets
-				content={block.content as unknown as ModuleProjetsContent}
-			/>
-		{:else if block.type === 'module-cta'}
-			<BlockModuleCta
-				content={block.content as unknown as ModuleCtaContent}
-			/>
-		{:else if block.type === 'module-partenaires'}
-			<BlockModulePartenaires
-				content={block.content as unknown as ModulePartenairesContent}
-			/>
-		{/if}
-	{/if}
-{/each}
+<Blocks blocks={page.body} theme={page.theme} />
