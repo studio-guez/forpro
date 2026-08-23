2fd27669180fffecf010e764eeaa4c0a1e32de91
OctoY <yann@octoplus.solutions>
2026-08-23 19:36:57 +0200

    Add Blocks component to handle various block types in Svelte

---
+++ website/src/lib/components/Blocks.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import BlockModuleTitreTexteImage from '$lib/components/BlockModuleTitreTexteImage.svelte';
+	import BlockModuleCases from '$lib/components/BlockModuleCases.svelte';
+	import BlockModuleGrilleImages from '$lib/components/BlockModuleGrilleImages.svelte';
+	import BlockModuleInfosPratiques from '$lib/components/BlockModuleInfosPratiques.svelte';
+	import BlockModuleAgenda from '$lib/components/BlockModuleAgenda.svelte';
+	import BlockModuleProjets from '$lib/components/BlockModuleProjets.svelte';
+	import BlockModuleCta from '$lib/components/BlockModuleCta.svelte';
+	import BlockModulePartenaires from '$lib/components/BlockModulePartenaires.svelte';
+	import type {
+		Block,
+		Theme,
+		ModuleTitreTexteImageContent,
+		ModuleCasesContent,
+		ModuleGrilleImagesContent,
+		ModuleInfosPratiquesContent,
+		ModuleAgendaContent,
+		ModuleProjetsContent,
+		ModuleCtaContent,
+		ModulePartenairesContent
+	} from '$lib/interfaces/page';
+
+	let { blocks, theme = 'default' }: { blocks: Block[]; theme?: Theme } = $props();
+</script>
+
+{#each blocks as block (block.id)}
+	{#if !block.isHidden}
+		{#if block.type === 'module-titre-texte-image'}
+			<BlockModuleTitreTexteImage
+				content={block.content as unknown as ModuleTitreTexteImageContent}
+				{theme}
+			/>
+		{:else if block.type === 'module-cases'}
+			<BlockModuleCases content={block.content as unknown as ModuleCasesContent} {theme} />
+		{:else if block.type === 'module-grille-images'}
+			<BlockModuleGrilleImages
+				content={block.content as unknown as ModuleGrilleImagesContent}
+				{theme}
+			/>
+		{:else if block.type === 'module-infos-pratiques'}
+			<BlockModuleInfosPratiques content={block.content as unknown as ModuleInfosPratiquesContent} />
+		{:else if block.type === 'module-agenda'}
+			<BlockModuleAgenda content={block.content as unknown as ModuleAgendaContent} />
+		{:else if block.type === 'module-projets'}
+			<BlockModuleProjets content={block.content as unknown as ModuleProjetsContent} />
+		{:else if block.type === 'module-cta'}
+			<BlockModuleCta content={block.content as unknown as ModuleCtaContent} />
+		{:else if block.type === 'module-partenaires'}
+			<BlockModulePartenaires content={block.content as unknown as ModulePartenairesContent} />
+		{/if}
+	{/if}
+{/each}
