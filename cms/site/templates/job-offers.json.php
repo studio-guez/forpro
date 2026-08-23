c2dfe8dae76cd0f030abb6ab2a88bfb4cd5c9f73
OctoY <yann@octoplus.solutions>
2026-08-23 21:30:18 +0200

    Add job offer JSON templates and update TypeScript interfaces for enhanced job offer data handling

--- cms/site/templates/job-offers.json.php
+++ cms/site/templates/job-offers.json.php
@@ -12,6 +12,13 @@ $json['cover'] = Utils::getJsonEncodeImageDataOrNull($page->cover()->toFile());
 
 $json['shortDesc'] = $page->shortDesc()->value();
 
+// All terms in their CMS-defined order, so the frontend can order filters accordingly.
+$json['domains']            = Utils::getTaxonomyTerms('domains');
+$json['jobOfferCategories'] = Utils::getTaxonomyTerms('job-offer-categories');
+
+$json['jobOffers'] = array_values($page->children()->listed()
+    ->map(fn($offer) => Utils::getJobOfferCardData($offer))->data());
+
 $json['body'] = Utils::getBodyBlocks($page->body());
 
 $json['seo'] = Utils::getSeoDataFromPage($page);
