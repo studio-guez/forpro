cca6366101bf139d225f4ea671030fe9a2a71966
OctoY <yann@octoplus.solutions>
2026-08-25 10:44:49 +0200

    Add ResourceCard component for displaying resource details with image and description

---
+++ website/src/lib/components/ui/ResourceCard.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Img from '$lib/components/ui/Img.svelte';
+	import type { ResourceCard } from '$lib/interfaces/page';
+
+	interface Props {
+		resource: ResourceCard;
+		headingTag?: string;
+	}
+
+	let { resource, headingTag = 'h3' }: Props = $props();
+</script>
+
+<article>
+	<div class="relative rounded-2xl overflow-hidden aspect-16/9">
+		{#if resource.image}
+			<Img
+				image={resource.image}
+				alt={resource.image.alt ?? resource.title}
+				sizes="(min-width: 768px) 33vw, 80vw"
+				class="w-full h-full object-cover"
+			/>
+		{/if}
+	</div>
+
+	<svelte:element this={headingTag} class="text-h4 mt-5">{resource.title}</svelte:element>
+	{#if resource.shortDesc}
+		<div class="prose text-body-2 mt-3">
+			{@html resource.shortDesc}
+		</div>
+	{/if}
+</article>
