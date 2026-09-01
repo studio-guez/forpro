f0c6fb05a54b581559f6636a3fbd4cde02c8328c
OctoY <yann@octoplus.solutions>
2026-09-01 15:30:22 +0200

    refactor: update decoPositions for improved layout and adjust text styles for better readability in ThreeElements component

--- website/src/lib/components/ui/ThreeElements.svelte
+++ website/src/lib/components/ui/ThreeElements.svelte
@@ -37,25 +37,25 @@
 		decoShapes = [ShapeInfosPratiquesDeco2, ShapeInfosPratiquesDeco1, ShapeInfosPratiquesDeco3],
 		rotations = [-6, 12, -6],
 		decoPositions = [
-			'left-[44%] top-[8%] w-32 lg:w-50',
-			'left-[12%] bottom-[-8%] w-44 lg:w-60',
-			'right-[6%] bottom-[-10%] w-28 lg:w-44',
+			'left-[24%] top-[12%] max-xl:-rotate-30 xl:left-[44%] xl:top-[8%] w-1/4 xl:w-1/6',
+			'left-[18%] bottom-[4%] max-xl:rotate-10 xl:left-[12%] xl:bottom-[-8%] w-1/3 xl:w-2/9',
+			'right-[8%] max-xl:top-[42%] max-xl:-rotate-15 xl:right-[6%] xl:bottom-[-10%] w-1/5 xl:w-1/6',
 		],
 		class: className = '',
 	}: Props = $props();
 </script>
 
-<div class="relative grid grid-cols-1 lg:grid-cols-3 gap-10 items-start {className}">
+<div class="relative grid grid-cols-2 xl:grid-cols-3 xl:gap-10 items-start {className}">
 	{#if decoColor}
 		{#each decoShapes as DecoShape, i (i)}
-			<div class="hidden lg:block absolute {decoPositions[i]}" style:color={decoColor}>
+			<div class="absolute {decoPositions[i]}" style:color={decoColor}>
 				<DecoShape class="w-full h-auto" />
 			</div>
 		{/each}
 	{/if}
 	{#each elements as element, i (i)}
 		{@const Shape = shapes[i % shapes.length]}
-		<div class="relative aspect-15/13 w-full max-w-90 mx-auto" class:lg:mt-48={i === 1}>
+		<div class="relative aspect-15/13 w-full max-w-90 mx-auto" class:xl:mt-48={i === 1} class:max-xl:col-start-2={i === 0 || i === 2} class:max-xl:row-start-2={i === 1} class:max-xl:row-start-3={i === 2}>
 			<div class="absolute -inset-1/8" style:color={blobColor}>
 				<Shape class="w-full h-full" />
 			</div>
@@ -64,8 +64,8 @@
 				style:transform="rotate({rotations[i % rotations.length]}deg)"
 				style:color={textColor}
 			>
-				<h3 class="text-h4 font-bold">{element.title}</h3>
-				<p class="text-body-1 font-bold mt-6 whitespace-pre-line">{element.description}</p>
+				<h3 class="text-lg lg:text-3xl font-bold">{element.title}</h3>
+				<p class="text-base lg:text-2xl lg:font-bold mt-3 lg:mt-6 whitespace-pre-line">{element.description}</p>
 			</div>
 		</div>
 	{/each}
