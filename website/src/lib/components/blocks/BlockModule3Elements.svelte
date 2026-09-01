522c7491f4a053c54fb7616935ea9810c5f42dfa
OctoY <yann@octoplus.solutions>
2026-09-01 18:22:24 +0200

    refactor: integrate new shape components for enhanced theme support in BlockModule3Elements

--- website/src/lib/components/blocks/BlockModule3Elements.svelte
+++ website/src/lib/components/blocks/BlockModule3Elements.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Module3ElementsContent, Theme } from '$lib/interfaces/page';
 	import { getThemeColors } from '$lib/utils/themeColors';
+	import { getThemeElementShapes } from '$lib/utils/themeShapes';
 	import Card from '$lib/components/ui/Card.svelte';
 	import ThreeElements from '$lib/components/ui/ThreeElements.svelte';
 
@@ -13,6 +14,7 @@
 
 	const colors = $derived(getThemeColors(theme, content.variant));
 	const inverted = $derived(content.variant === 'inverted');
+	const shapes = $derived(getThemeElementShapes(theme));
 </script>
 
 {#if content.elements.length > 0}
@@ -24,12 +26,15 @@
 		titleBackground={colors.titleBackground}
 		titleColor={colors.title}
 	>
-		<ThreeElements
-			elements={content.elements}
-			blobColor={inverted ? colors.invertedElementsBlobColor : colors.bgContrast}
-			textColor={inverted ? colors.invertedElementsTextColor : colors.surfaceText}
-			decoColor={inverted ? colors.invertedElementsTextColor : colors.bgContrast}
-			class="mt-12 lg:mt-6"
-		/>
+		<div class:lg:pb-18={inverted}>
+			<ThreeElements
+				elements={content.elements}
+				{shapes}
+				blobColor={inverted ? colors.invertedElementsBlobColor : colors.bgContrast}
+				textColor={inverted ? colors.invertedElementsTextColor : colors.surfaceText}
+				decoColor={inverted ? colors.invertedElementsTextColor : colors.bgContrast}
+				class="mt-12 lg:mt-6"
+			/>
+		</div>
 	</Card>
 {/if}
