fa16b6bec0b4f52df25e3c625da2f880c82593a2
OctoY <yann@octoplus.solutions>
2026-09-03 14:14:25 +0200

    feat: update MissionCard styles and improve accessibility

--- website/src/lib/components/ui/MissionCard.svelte
+++ website/src/lib/components/ui/MissionCard.svelte
@@ -13,22 +13,22 @@
 	const date = $derived(toDate(mission.date));
 </script>
 
-<article class="relative h-full flex flex-col gap-3 text-blue">
+<article class="relative h-full flex flex-col gap-3">
 	<svelte:element this={headingTag} class="text-h4">
-		<a href={mission.url} class="hover:opacity-70 transition after:absolute after:inset-0">
+		<a href={mission.url} class="hover:opacity-50 transition after:absolute after:inset-0">
 			{mission.title}
 		</a>
 	</svelte:element>
 
-	<p class="text-label text-grey-dark">
+	<p class="text-label">
 		<time datetime={mission.date}>{date ? formatShortDate(date) : mission.date}</time>
 		<span aria-hidden="true">·</span>
 		{mission.location}
 	</p>
 
 	{#if mission.shortDesc}
-		<div class="prose text-body-2 text-grey-dark">{@html mission.shortDesc}</div>
+		<div class="prose text-body-2">{@html mission.shortDesc}</div>
 	{/if}
 
-	<TermTags terms={mission.terms} label="Domaines" class="mt-auto pt-2" />
+	<TermTags terms={mission.terms} label="Domaines" size="md" />
 </article>
