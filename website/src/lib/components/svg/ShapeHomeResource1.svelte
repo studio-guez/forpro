932bf07f8bdce0622e8a3a5d82df42729f8e3bdb
OctoY <yann@octoplus.solutions>
2026-09-13 15:56:02 +0200

    feat: add SVG components for home resources

---
+++ website/src/lib/components/svg/ShapeHomeResource1.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	interface Props {
+		class?: string;
+	}
+
+	let { class: className = '' }: Props = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="185"
+	height="188"
+	viewBox="0 0 185 188"
+	fill="none"
+	aria-hidden="true"
+	class={className}
+>
+	<path
+		d="M146.626 39.7402C124.778 1.89785 76.6466 -11.2231 39.096 10.4568C1.54536 32.1366 -11.1573 80.3802 10.691 118.223L56.1972 91.9495L139.671 236.53L184.594 210.594L101.12 66.0132L146.626 39.7402Z"
+		fill="currentColor"
+	/>
+</svg>
