24a26899138af2711da351c9116b087a9fe5b5e4
OctoY <yann@octoplus.solutions>
2026-09-14 10:22:48 +0200

    fix: update text styles for improved consistency across components

--- website/src/lib/utils/sectionStyles.ts
+++ website/src/lib/utils/sectionStyles.ts
@@ -13,4 +13,4 @@
 export const LABELLED_SECTION = 'grid grid-cols-1 lg:grid-cols-4 gap-x-6 gap-y-4 lg:gap-y-9';
 
 export const LABELLED_SECTION_ITEMS =
-	'lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4 lg:gap-y-12';
+	'text-body-2 lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4 lg:gap-y-12';
