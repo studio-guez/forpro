2ef38584dc1d80c0edd763059e0fbb8d2a83e39e
OctoY <yann@octoplus.solutions>
2026-09-16 14:37:04 +0200

    fix: adjust padding in PageIntro component for improved layout

--- website/src/lib/components/blocks/PageIntro.svelte
+++ website/src/lib/components/blocks/PageIntro.svelte
@@ -46,7 +46,7 @@
 	const hasCta = $derived(!!cta?.label && !!cta?.url);
 </script>
 
-<section class="px-13 lg:px-24 xl:px-39 relative max-lg:py-9" aria-labelledby="page-intro-title">
+<section class="px-13 lg:px-24 xl:px-39 relative max-lg:py-3" aria-labelledby="page-intro-title">
 	{#if parentPage}
 		<div class="mb-6 mt-[calc(-1.5rem-1lh)] -ml-8 lg:-ml-15 xl:-ml-30">
 			<BackLink {parentPage} color={themeColor} />
